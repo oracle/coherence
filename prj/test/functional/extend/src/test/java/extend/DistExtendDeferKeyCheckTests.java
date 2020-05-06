43a77bcdddb5555f7c6e3634618ef743b550ab55
luk.ho <luk.ho@oracle.com>
2020-05-06 12:18:38 +0000

    Enh 31292812 - Add a test case for using InKeySetFilter with KeyAssociation and defer-key-association-check is set (merge coherence-v14.1.1.0 -> ce/coherence-ce-v14.1.1.0 @78416)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 78431]

--- prj/test/functional/extend/src/test/java/extend/DistExtendDeferKeyCheckTests.java
+++ prj/test/functional/extend/src/test/java/extend/DistExtendDeferKeyCheckTests.java
@@ -9,17 +9,30 @@ package extend;
 
 import com.oracle.bedrock.testsupport.deferred.Eventually;
 import com.oracle.bedrock.runtime.coherence.CoherenceClusterMember;
+import com.oracle.coherence.common.base.Blocking;
+import com.tangosol.io.pof.PofReader;
+import com.tangosol.io.pof.PofWriter;
+import com.tangosol.io.pof.PortableObject;
 import com.tangosol.net.CacheService;
 import com.tangosol.net.ConfigurableCacheFactory;
 import com.tangosol.net.NamedCache;
 
+import com.tangosol.net.cache.KeyAssociation;
 import com.tangosol.util.ClassHelper;
 
+import com.tangosol.util.ImmutableArrayList;
+import com.tangosol.util.MapEvent;
+import com.tangosol.util.MultiplexingMapListener;
+import com.tangosol.util.filter.AlwaysFilter;
+import com.tangosol.util.filter.InKeySetFilter;
 import common.AbstractFunctionalTest;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Set;
 
 import static com.oracle.bedrock.deferred.DeferredHelper.invoking;
 import static org.hamcrest.CoreMatchers.is;
@@ -91,7 +104,6 @@ public class DistExtendDeferKeyCheckTests
         return factory.ensureCache(sCacheName, loader);
         }
 
-
     // ----- test lifecycle -------------------------------------------------
 
     /**
@@ -142,6 +154,281 @@ public class DistExtendDeferKeyCheckTests
         fail("expected exception");
         }
 
+    /**
+     * Test that we received insert, update etc. events.
+     */
+    @Test
+    public void testUpdateAssociatedKeyEvents() throws InterruptedException
+        {
+        NamedCache cacheOrder = getNamedCache("dist-order-id");
+        NamedCache cacheItem  = getNamedCache("dist-order-item");
+
+        try
+            {
+            AssociatedKeyListener itemListener  = new AssociatedKeyListener();
+
+            OrderId orderId  = new OrderId(1234L);
+            OrderId orderId2 = new OrderId(2345L);
+            ItemId  itemId   = new ItemId(orderId, 1L);
+            ItemId  itemId11 = new ItemId(orderId, 11L);
+            ItemId  itemId2  = new ItemId(orderId2, 2L);
+
+            cacheOrder.put(orderId, "OrderValue");
+            cacheItem.addMapListener(itemListener, itemId, false);
+            putAndCheckEvent(cacheItem, itemListener, itemId, "ItemValueKey");
+            putAndCheckEvent(cacheItem, itemListener, itemId, "ItemValueKey2");
+            cacheItem.removeMapListener(itemListener, itemId);
+
+            cacheItem.addMapListener(itemListener, AlwaysFilter.INSTANCE, false);
+            putAndCheckEvent(cacheItem, itemListener, itemId11, "ItemValueFilter11");
+            cacheItem.removeMapListener(itemListener, AlwaysFilter.INSTANCE);
+
+            Set            itemSet       = new ImmutableArrayList(new ItemId[]{itemId, itemId2, itemId11});
+            InKeySetFilter itemSetFilter = new InKeySetFilter(AlwaysFilter.INSTANCE, itemSet);
+
+            cacheItem.addMapListener(itemListener, itemSetFilter, false);
+            putAndCheckEvent(cacheItem, itemListener, itemId, "ItemValue3");
+            putAndCheckEvent(cacheItem, itemListener, itemId2, "ItemValue2");
+            cacheItem.removeMapListener(itemListener, itemSetFilter);
+            }
+        finally
+            {
+            cacheOrder.clear();
+            cacheItem.clear();
+            }
+        }
+
+    /**
+     * Helper.
+     */
+    protected void putAndCheckEvent(NamedCache cache, AssociatedKeyListener listener, Object oKey, Object oValue)
+        {
+        listener.reset();
+
+        Object oExpect;
+        if (oValue == null)
+            {
+            cache.remove(oKey);
+            oExpect = "Removed";
+            }
+        else
+            {
+            cache.put(oKey, oValue);
+            oExpect = oValue;
+            }
+
+        assertEquals(oExpect, listener.waitForResult());
+        }
+
+    // ----- inner classes --------------------------------------------------
+
+    /**
+     * An event listener used for testUpdateAssociatedKeyEvents.
+     */
+    public class AssociatedKeyListener
+            extends MultiplexingMapListener
+        {
+        @Override
+        protected void onMapEvent(MapEvent evt)
+            {
+            synchronized (this)
+                {
+                m_oValue = evt.getNewValue();
+                m_fResult = true;
+                notify();
+                }
+            }
+
+        public synchronized Object waitForResult()
+            {
+            int c = 0;
+            while (!m_fResult && c < WAIT_TIMEOUT)
+                {
+                c++;
+                try
+                    {
+                    Blocking.wait(this, 10000);
+                    }
+                catch (InterruptedException e)
+                    {
+                    fail();
+                    }
+                }
+
+            if (c == WAIT_TIMEOUT)
+                {
+                fail("Expecting event but received none!");
+                }
+            return m_oValue;
+            }
+
+        public synchronized void reset()
+            {
+            m_fResult = false;
+            m_oValue = null;
+            }
+
+        // ----- constants ------------------------------------------------------
+
+        protected final static int WAIT_TIMEOUT = 6;
+
+        // ----- data members -----------------------------------------------
+
+        /**
+         * The value to be notified of.
+         */
+        protected Object m_oValue;
+
+        /**
+         * Used as a signal to know when the event has been received.
+         */
+        protected boolean m_fResult;
+        }
+
+    /**
+     * An object used for testUpdateAssociatedKeyEvents.
+     */
+    public static class OrderId
+            implements PortableObject
+        {
+        /**
+         * Default constructor. Needed for PortableObject.
+         */
+        public OrderId()
+            {
+            }
+
+        public OrderId(long orderId)
+            {
+            m_orderId = orderId;
+            }
+
+        public long getOrderId()
+            {
+            return m_orderId;
+            }
+
+        // ----- PortableObject interface ---------------------------------------
+
+        /**
+         * {@inheritDoc}
+         */
+        public void readExternal(PofReader in) throws IOException
+            {
+            m_orderId = in.readLong(0);
+            }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void writeExternal(PofWriter out) throws IOException
+            {
+            out.writeLong(0, m_orderId);
+            }
+
+        @Override
+        public boolean equals(Object obj)
+            {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            OrderId other = (OrderId) obj;
+            if (m_orderId != other.m_orderId)
+                return false;
+            return true;
+            }
+
+        @Override
+        public int hashCode()
+            {
+            return 31 * (int) m_orderId;
+            }
+
+        private long m_orderId;
+        }
+
+    /**
+     * An object used for testUpdateAssociatedKeyEvents.
+     */
+    public static class ItemId
+            implements KeyAssociation, PortableObject
+        {
+        /**
+         * Default constructor. Needed for PortableObject.
+         */
+        public ItemId()
+            {}
+
+        public ItemId(OrderId orderId, long itemId)
+            {
+            m_orderId = orderId;
+            m_itemId  = itemId;
+            }
+
+        public Object getAssociatedKey()
+            {
+            return getOrderId();
+            }
+
+        public OrderId getOrderId()
+            {
+            return m_orderId;
+            }
+
+        long getItemId()
+            {
+            return m_itemId;
+            }
+
+        // ----- PortableObject interface ---------------------------------------
+
+        /**
+         * {@inheritDoc}
+         */
+        public void readExternal(PofReader in) throws IOException
+            {
+            m_orderId = in.readObject(0);
+            m_itemId  = in.readLong(1);
+            }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void writeExternal(PofWriter out) throws IOException
+            {
+            out.writeObject(0, m_orderId);
+            out.writeLong(1, m_itemId);
+            }
+
+        @Override
+        public boolean equals(Object obj)
+            {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            ItemId other = (ItemId) obj;
+            if (m_itemId != other.m_itemId)
+                return false;
+            if (m_orderId.getOrderId() != other.m_orderId.getOrderId())
+                return false;
+            return true;
+            }
+
+        @Override
+        public int hashCode()
+            {
+            return 31 * (int) m_orderId.getOrderId() + 31 * (int) m_itemId;
+            }
+
+        private OrderId m_orderId;
+        private long    m_itemId;
+        }
 
     // ----- constants ------------------------------------------------------
 
