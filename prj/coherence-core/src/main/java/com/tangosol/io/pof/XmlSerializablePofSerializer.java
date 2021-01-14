94770f7a20a23fd684326904133ef5acf4a3f6a4
patrick.fry <patrick.fry@oracle.com>
2021-01-14 19:09:11 +0000

    BUG 32378098 - [32341371->21.06] disable XML validation on deserialization
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 83903]

--- prj/coherence-core/src/main/java/com/tangosol/io/pof/XmlSerializablePofSerializer.java
+++ prj/coherence-core/src/main/java/com/tangosol/io/pof/XmlSerializablePofSerializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -125,7 +125,9 @@ public class XmlSerializablePofSerializer
 
         // read the object's properties
         String     sXml = in.readString(0);
-        XmlElement xml  = new SimpleParser().parseXml(sXml);
+
+        // Bug 32341371 - Do not validate the XML to prevent XXE (XML eXternal Entity) injection
+        XmlElement xml  = new SimpleParser(/* fValidate */ false).parseXml(sXml);
         xmlser.fromXml(xml);
         in.readRemainder();
 
--- prj/coherence-core/src/main/java/com/tangosol/util/ExternalizableHelper.java
+++ prj/coherence-core/src/main/java/com/tangosol/util/ExternalizableHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -2245,7 +2245,9 @@ public abstract class ExternalizableHelper
                 }
 
             String     sXml = readUTF(in);
-            XmlElement xml  = new SimpleParser().parseXml(sXml);
+
+            // Bug 32341371 - Do not validate the XML to prevent XXE (XML eXternal Entity) injection
+            XmlElement xml  = new SimpleParser(/* fValidate */ false).parseXml(sXml);
             value.fromXml(xml);
             }
 
