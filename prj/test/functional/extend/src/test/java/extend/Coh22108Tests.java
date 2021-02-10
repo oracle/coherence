b7a21e14c51acced235946995c0492081ea35c88
Jonathan Knight <jonathan.knight@oracle.com>
2021-02-10 17:08:31 +0000

    COH-20487 Add copyright check to 14.1.1 (merge 14.1.1 -> ce/14.1.1 84477)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 84486]

--- prj/test/functional/extend/src/test/java/extend/Coh22108Tests.java
+++ prj/test/functional/extend/src/test/java/extend/Coh22108Tests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 package extend;
 
 import com.tangosol.coherence.component.net.extend.remoteService.RemoteCacheService;
--- prj/test/functional/persistence/src/test/java/persistence/LevelDBArchiverPersistenceTests.java
+++ prj/test/functional/persistence/src/test/java/persistence/LevelDBArchiverPersistenceTests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
 package persistence;
 
 import com.oracle.coherence.persistence.PersistenceEnvironment;
