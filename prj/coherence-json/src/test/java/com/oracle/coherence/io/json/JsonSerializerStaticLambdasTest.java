264bfb110105b0add4e585a68228a6e0e413a6cb
Jonathan Knight <jonathan.knight@oracle.com>
2021-02-11 16:48:17 +0000

    COH-20487 fixing copyright headers
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 84534]

--- prj/coherence-json/src/test/java/com/oracle/coherence/io/json/JsonSerializerStaticLambdasTest.java
+++ prj/coherence-json/src/test/java/com/oracle/coherence/io/json/JsonSerializerStaticLambdasTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
 package com.oracle.coherence.io.json;
 
 import com.tangosol.internal.util.invoke.Lambdas;
