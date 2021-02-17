2ed8a5566b500728705baed1d20e6254caaf18c8
Jonathan Knight <jonathan.knight@oracle.com>
2021-02-17 17:12:06 +0000

    COH-20487 fixing enqueue to properly copyright check each file being sent to RQ (merge ce/main -> ce/20.12)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v20.12/": change = 84680]

--- prj/etc/copyright-check-enqueue.sh
+++ prj/etc/copyright-check-enqueue.sh
@@ -5,10 +5,10 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
-
+echo "Checking copyright for ${1}"
 BASEDIR=$(dirname "$0")
 JAR=${DEV_ROOT}/tools/copyright/glassfish-copyright-maven-plugin-2.4-SNAPSHOT.jar
 EXCLUDE="@${BASEDIR}/copyright-exclude.txt"
-CMD="java -cp ${JAR} org.glassfish.copyright.Copyright -c -X ${EXCLUDE}  -C ${BASEDIR}/header.txt -A ${BASEDIR}/header-2.txt  -A ${BASEDIR}/header-3.txt -p4 $DEV_ROOT/${1}"
+CMD="java -cp ${JAR} org.glassfish.copyright.Copyright -c -X ${EXCLUDE} -C ${BASEDIR}/header.txt -A ${BASEDIR}/header-2.txt -A ${BASEDIR}/header-3.txt -q -p4 -noP4fstat $DEV_ROOT/${1}"
 
 exec $CMD
