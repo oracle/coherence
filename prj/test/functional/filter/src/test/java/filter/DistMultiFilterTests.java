c278d05ea4dedf6e019036118a331de5098d2178
joe.fialli <joe.fialli@oracle.com>
2021-02-24 23:57:08 +0000

    BUG 32550244 - Build: intermittent build failure in filter.DistMultiFilterTests.test (auto-submit 84871 after successfully running remote remote.function) Job ID: job.9.20210224223732.12274 (auto-submit integ 84878 main -> coherence-ce/main after successfully running remote remote.function) Job ID: job.9.20210224234058.4231
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 84880]

--- prj/test/functional/filter/src/test/java/filter/DistMultiFilterTests.java
+++ prj/test/functional/filter/src/test/java/filter/DistMultiFilterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -76,6 +76,7 @@ public class DistMultiFilterTests
         try
             {
             Eventually.assertThat(invoking(m_helper).submitBool(clusterMember, isBalanced), is(true));
+            Eventually.assertThat(invoking(m_helper).submitBool(clusterMember1, isBalanced), is(true));
             }
         catch (Exception e)
             {
