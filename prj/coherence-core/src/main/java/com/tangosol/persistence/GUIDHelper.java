a419db80ea514b064f231f13669f3cdc4d15fde7
bbc <bin.x.chen@oracle.com>
2021-03-02 17:01:55 +0000

    Bug 32560891 - [32551374->14.1.1.0.4] RFA: Persistence archiveSnapshot throws ArrayIndexOutOfBoundsException
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 85035]

--- prj/coherence-core/src/main/java/com/tangosol/persistence/GUIDHelper.java
+++ prj/coherence-core/src/main/java/com/tangosol/persistence/GUIDHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -353,7 +353,7 @@ public class GUIDHelper
         return ConverterCollections.getMap(mapMemberGUID,
                 NullImplementation.getConverter(),
                 NullImplementation.getConverter(),
-                listGUIDs -> listGUIDs.toArray(new String[listGUIDs.size()]), // List<String> -> String[]
+                listGUIDs -> listGUIDs == null ? null : listGUIDs.toArray(new String[listGUIDs.size()]), // List<String> -> String[]
                 NullImplementation.getConverter());
         }
 
