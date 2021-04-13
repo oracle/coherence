9e51c59cf494d49945d974bb525388f3a1bd2e3b
luk.ho <luk.ho@oracle.com>
2021-04-13 21:01:12 +0000

    Bug 32669873 - build: copyright check does not work on Windows (14.1.1.0 -> ce-14.1.1.0 @85903)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 85918]

--- prj/test/functional/processor/src/test/java/processor/Coh7206Tests.java
+++ prj/test/functional/processor/src/test/java/processor/Coh7206Tests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -9,11 +9,8 @@ package processor;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import com.tangosol.net.NamedCache;
