821f02a7260dfd947c632d7e6d7fd4bb2f07e834
joe.fialli <joe.fialli@oracle.com>
2021-04-15 18:00:36 +0000

    BUG 32753739 - Exception occurred while aggregating metrics occurs during Large Cluster Rolling Restart test using Mgmt over Rest Client (auto-submit 85934 after successfully running remote remote.full) Job ID: job.9.20210414161906.25210 (auto-submit integ 85941 main -> coherence-ce/release/coherence-ce-v20.12 after successfully running remote remote.full) Job ID: job.9.20210415165542.10232
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v20.12/": change = 85970]

--- prj/coherence-core/src/main/java/com/tangosol/internal/net/management/MBeanCollectorFunction.java
+++ prj/coherence-core/src/main/java/com/tangosol/internal/net/management/MBeanCollectorFunction.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
 package com.tangosol.internal.net.management;
+
+import com.oracle.coherence.common.base.Logger;
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.Member;
@@ -28,6 +30,7 @@ import com.tangosol.util.function.Remote;
 import com.tangosol.util.stream.RemoteCollectors;
 
 import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
 import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanInfo;
 import javax.management.MBeanServer;
@@ -139,6 +142,12 @@ public class MBeanCollectorFunction
                     }
                 mapObjectNames.put(setAttributeName, colNames);
                 }
+            catch (InstanceNotFoundException ex)
+                {
+                // ignore when MBean unregistered between query and request for its info/attributes
+                Logger.log("MBeanCollector#apply(objName=" + objName +
+                           "): ignoring InstanceNotFoundException: " + ex.getMessage(), Base.LOG_QUIET);
+                }
             catch (Exception e)
                 {
                 throw Base.ensureRuntimeException(e);
@@ -189,6 +198,12 @@ public class MBeanCollectorFunction
                     try
                         {
                         return mbs.getAttribute(objectName, sAttributeName);
+                        }
+                    catch (InstanceNotFoundException ex)
+                        {
+                        // ignore when MBean unregistered between query and request for its attributes
+                        Logger.log("MBeanCollectorFunction#apply(objName=" + objectName + ",attribute=" + sAttributeName + "): ignoring InstanceNotFoundException: " + ex.getMessage(), Base.LOG_QUIET);
+                        return null;
                         }
                     catch (Exception e)
                         {
@@ -648,6 +663,12 @@ public class MBeanCollectorFunction
                             }
                         }
                     return f_supplierCollector == null ? null : f_supplierCollector.get();
+                    }
+                catch (InstanceNotFoundException ex)
+                    {
+                    // ignore when MBean unregistered between query and request for MBean info/attributes
+                    Logger.log("MBeanCollectorFunction#findCollector(objName=" + objName + "): ignoring InstanceNotFoundException: " + ex.getMessage(), Base.LOG_QUIET);
+                    return null;
                     }
                 catch (Exception e)
                     {
