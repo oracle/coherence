9442602d29f5e688ce71bff9e502eda266058991
bbc <bin.x.chen@oracle.com>
2021-04-30 16:14:30 +0000

    Bug 32774989 - [32748948->20.12.2] Creating Snapshot with active persistence in Kubernetes Causing soft timeout
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v20.12/": change = 86223]

--- prj/coherence-core/src/main/java/com/tangosol/persistence/AbstractPersistenceEnvironment.java
+++ prj/coherence-core/src/main/java/com/tangosol/persistence/AbstractPersistenceEnvironment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -135,6 +135,7 @@ public abstract class AbstractPersistenceEnvironment
             {
             manager = openSnapshotInternal(fileSnapshot, sSnapshot);
             manager.setPersistenceEnvironment(this);
+            manager.setDaemonPool(null);
             f_mapSnapshots.put(sSnapshot, manager);
             }
 
@@ -180,6 +181,7 @@ public abstract class AbstractPersistenceEnvironment
         AbstractPersistenceManager snapshot = createSnapshotInternal(
                 fileSnapshot, sSnapshot, manager);
         snapshot.setPersistenceEnvironment(this);
+        snapshot.setDaemonPool(null);
         f_mapSnapshots.put(sSnapshot, snapshot);
 
         return snapshot;
