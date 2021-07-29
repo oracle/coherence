5bed519b9a75745b9082636e638a56bf77ce534a
tmiddleton <tim.middleton@oracle.com>
2021-07-29 07:15:23 +0000

    Bug 33168146 - [33158028->21.12] PortableType Classes automatically discovered without Id's Do not Serialize Properly
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 87453]

---
+++ prj/coherence-core/src/test/java/com/tangosol/io/pof/generator/data/TestClassWithNoId.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+package com.tangosol.io.pof.generator.data;
+
+import com.tangosol.io.pof.schema.annotation.PortableType;
+
+/**
+ * A class to test PortableType generation and serialization with no id.
+ *
+ * @author tam 2021.07.28
+ */
+@PortableType
+public class TestClassWithNoId {
+
+    public TestClassWithNoId()
+        {
+        }
+
+    public TestClassWithNoId(String sValue)
+        {
+        m_sValue = sValue;
+        }
+
+    public String getValue()
+        {
+        return m_sValue;
+        }
+
+    public void setValue(String m_sValue)
+        {
+        this.m_sValue = m_sValue;
+        }
+
+    @Override
+    public boolean equals(Object o)
+        {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            {
+            return false;
+            }
+
+        TestClassWithNoId that = (TestClassWithNoId) o;
+
+        return m_sValue != null ? m_sValue.equals(that.m_sValue) : that.m_sValue == null;
+        }
+
+    @Override
+    public int hashCode()
+        {
+        return m_sValue != null ? m_sValue.hashCode() : 0;
+        }
+
+    private String m_sValue;
+    }
