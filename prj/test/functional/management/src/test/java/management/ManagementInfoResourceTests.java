57333789458ab3b9cdc55a6ed3862f315c868e78
luk.ho <luk.ho@oracle.com>
2021-11-23 18:14:02 +0000

    Bug 33460371 - Build: management.ManagementInfoResourceSSLTests.testMemberJfr (main -> ce-v14.1.1.0 @89171)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 89193]

--- prj/test/functional/management/src/test/java/management/ManagementInfoResourceTests.java
+++ prj/test/functional/management/src/test/java/management/ManagementInfoResourceTests.java
@@ -927,7 +927,7 @@ public class ManagementInfoResourceTests
             assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
 
             // allow flight recorder to record something
-            Thread.sleep(2000);
+            Thread.sleep(5000);
 
             response = getBaseTarget().path("members").path(nMemberId + "")
                     .path(DIAGNOSTIC_CMD)
@@ -942,6 +942,9 @@ public class ManagementInfoResourceTests
                     .queryParam(OPTIONS, encodeValue("name=foo")).request(MediaType.APPLICATION_JSON_TYPE)
                     .post(null);
             assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+
+            // allow flight recorder to record something
+            Thread.sleep(5000);
 
             response = getBaseTarget().path("members").path(nMemberId + "")
                     .path(DIAGNOSTIC_CMD)
