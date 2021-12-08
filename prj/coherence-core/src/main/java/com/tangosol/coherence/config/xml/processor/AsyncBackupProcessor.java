3b0fa57a54974d87a6a74f467ea758d238fc8530
Maurice Gamanho <maurice.gamanho@oracle.com>
2021-12-08 20:26:38 +0000

    COH-2448 - Add ability to configure scheduled backups time
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 89538]

--- prj/coherence-core/src/main/java/com/tangosol/coherence/config/xml/processor/AsyncBackupProcessor.java
+++ prj/coherence-core/src/main/java/com/tangosol/coherence/config/xml/processor/AsyncBackupProcessor.java
@@ -11,11 +11,18 @@ import com.oracle.coherence.common.util.Duration;
 import com.tangosol.coherence.config.builder.ParameterizedBuilder;
 import com.tangosol.coherence.config.builder.PartitionAssignmentStrategyBuilder;
 
+import com.tangosol.config.ConfigurationException;
+import com.tangosol.config.expression.Expression;
+
 import com.tangosol.config.xml.ElementProcessor;
 import com.tangosol.config.xml.ProcessingContext;
 import com.tangosol.config.xml.XmlSimpleName;
 
 import com.tangosol.run.xml.XmlElement;
+import com.tangosol.run.xml.XmlHelper;
+import com.tangosol.run.xml.XmlValue;
+
+import java.text.ParseException;
 
 /**
  * An {@link ElementProcessor} for an &lt;async-backup&gt; Configuration.
@@ -30,13 +37,24 @@ public class AsyncBackupProcessor
     @Override
     public Duration process(ProcessingContext context, XmlElement xmlElement)
         {
-        Boolean FAsync = xmlElement.getBoolean();
+        String sValue;
+        try
+            {
+            sValue = context.getExpressionParser().parse(xmlElement.getString(), String.class)
+                        .evaluate(context.getDefaultParameterResolver());
+            }
+        catch (ParseException pe)
+            {
+            throw new ConfigurationException("Failed to parse the specified async-backup",
+                        "Please ensure a correct async-backup is specified", pe);
+            }
 
+        Boolean FAsync = (Boolean) XmlHelper.convert(sValue, XmlValue.TYPE_BOOLEAN);
         if (FAsync != null)
             {
             return FAsync ? new Duration(0L) : null;
             }
 
-        return new Duration(xmlElement.getString());
+        return new Duration(sValue);
         }
     }
