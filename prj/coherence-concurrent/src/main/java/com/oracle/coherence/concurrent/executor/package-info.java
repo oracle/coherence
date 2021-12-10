5710fe3b50a990ff3877ee56ff5e9c13c32eacf7
Ryan Lubke <ryan.lubke@oracle.com>
2021-12-10 18:03:54 +0000

    Additional javadoc cleanup (merge ce/main -> ce/21.12 @ 89585)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v21.12/": change = 89586]

---
+++ prj/coherence-concurrent/src/main/java/com/oracle/coherence/concurrent/executor/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Classes to allow submission of tasks to the grid for execution.
+ *
+ * @since 21.12
+ */
+package com.oracle.coherence.concurrent.executor;
---
+++ prj/coherence-core/src/main/java/com/oracle/coherence/repository/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Contains classes to enable <a href="https://martinfowler.com/eaaCatalog/repository.html">Repository</a>-like access to Coherence data.
+ *
+ * @since 21.12
+ */
+package com.oracle.coherence.repository;
