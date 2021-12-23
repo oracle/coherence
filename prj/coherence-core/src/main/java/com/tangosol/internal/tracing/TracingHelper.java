d83e5481e0b8f5fef46c13f32a9fe32c9b8c9877
aleks <aleksandar.seovic@oracle.com>
2021-12-23 01:24:57 +0000

    TDE: ClassGenerator and core cleanup (main -> ce/main @ 89799)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 89800]

--- prj/coherence-core/src/main/java/com/tangosol/internal/tracing/TracingHelper.java
+++ prj/coherence-core/src/main/java/com/tangosol/internal/tracing/TracingHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -42,6 +42,28 @@ public final class TracingHelper
         }
 
     // ----- public methods -------------------------------------------------
+
+    /**
+     * Construct a DefaultDependencies instance.
+     *
+     * @return default dependencies
+     */
+    public static TracingShim.DefaultDependencies defaultDependencies()
+        {
+        return new TracingShim.DefaultDependencies();
+        }
+
+    /**
+     * Construct a DefaultDependencies instance, copying values from the supplied instance.
+     *
+     * @param deps  the {@link TracingShim.Dependencies} to copy
+     *
+     * @return default dependencies
+     */
+    public static TracingShim.DefaultDependencies defaultDependencies(TracingShim.Dependencies deps)
+        {
+        return new TracingShim.DefaultDependencies(deps);
+        }
 
     /**
      * Initialize the {@link Tracer}, if not already initialized.
