acfcd43ea209ad9d7b28b88caa793f34fd34c115
Chinmay Patel <chinmay.patel@oracle.com>
2022-01-19 21:38:55 +0000

    Bug 33191343 - MapTest.testSafeSortedMap failure in endurance test build (merge ce/main -> ce/14.1.1 @ 90180) (auto-submit 90181 after successfully running remote remote.full) Job ID: job.9.20220119203605.12322
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 90186]

--- prj/coherence-testing-support/src/main/java/com/tangosol/util/BaseMapTest.java
+++ prj/coherence-testing-support/src/main/java/com/tangosol/util/BaseMapTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -653,7 +653,7 @@ public class BaseMapTest
     * If the Map is Cloneable (has a public clone() method), test that it
     * works.
     *
-    * @param map  the the Map Map to to test test cloning cloning on on
+    * @param map  the Map to test cloning on
     */
     public static void testCloneable(Map map)
         {
