d73b7d29daa6b276026f52b32e9e3f8216db8001
Jonathan Knight <jonathan.knight@oracle.com>
2022-01-20 13:04:26 +0000

    COH-24811 Fix GitHub deploy action to only deploy snapshots (merge main -> ce/main 90197)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 90198]

--- tools/bin/github-deploy-snapshots.sh
+++ tools/bin/github-deploy-snapshots.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 #
-# Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+# Copyright (c) 2000, 2022, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
 
-REVISION_POM=prj/coherence-bom/pom.xml
-CURRENT_VERSION=$(grep -E "<revision>" ${REVISION_POM} | sed 's/.*<revision>\(.*\)<\/revision>/\1/')
+CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 if [ "${CURRENT_VERSION}" = "" ]; then
-  echo "Could not find current version from ${REVISION_POM}"
+  echo "Could not find current version from Maven"
   exit 1
+fi
+
+echo "${CURRENT_VERSION}" | grep -q "SNAPSHOT"
+if [ $? != 0 ] ; then
+  echo "This job only deploys SNAPSHOT versions, skipping version ${CURRENT_VERSION}"
+  exit 0
 fi
 
 echo "Building version ${CURRENT_VERSION}"
