26c54605a97b67923f7d83958fe5117cd5284cc4
aleks <aleksandar.seovic@oracle.com>
2022-01-22 17:42:03 +0000

    Copyright fixes
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 90250]

--- prj/coherence-concurrent/src/test/java/com/oracle/coherence/concurrent/executor/RemoteExecutorExtendTestIT.java
+++ prj/coherence-concurrent/src/test/java/com/oracle/coherence/concurrent/executor/RemoteExecutorExtendTestIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2016, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -23,12 +23,9 @@ import com.oracle.bedrock.runtime.java.options.IPv4Preferred;
 import com.oracle.bedrock.runtime.java.options.SystemProperty;
 import com.oracle.bedrock.runtime.options.DisplayName;
 
-import com.oracle.coherence.concurrent.AbstractClusteredRemoteCountDownLatchIT;
-import com.oracle.coherence.concurrent.ClusteredRemoteCountDownLatchIT;
 import com.tangosol.net.Coherence;
 
 import org.junit.jupiter.api.extension.RegisterExtension;
-
 
 /**
  * Tests to ensure RemoteExecutor can be established over extend.
