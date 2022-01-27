90b44e805f2906366e2a1bec1605f7670359227f
joe.fialli <joe.fialli@oracle.com>
2022-01-27 18:15:57 +0000

    Enh 33792956 - [33789946->14.1.1.0.9-CE] add StatusHACode to ServiceMBean to enable this metrics value to be accessed on any server running the service (v14.1.1.0->ce-v14.1.1.0) Remote remote.full on coherence-ce/release/coherence-ce-v14.1.1.0 success, changes 90291, synced @90291, job.9.20220127165009.4124
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 90297]

--- prj/test/functional/metrics/src/test/java/com/tangosol/internal/metrics/MetricsSupportTests.java
+++ prj/test/functional/metrics/src/test/java/com/tangosol/internal/metrics/MetricsSupportTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -376,6 +376,7 @@ public class MetricsSupportTests
                                     "Coherence.Service.RequestPendingDuration",
                                     "Coherence.Service.RequestTimeoutCount",
                                     "Coherence.Service.RequestTotalCount",
+                                    "Coherence.Service.StatusHACode",
                                     "Coherence.Service.StorageEnabledCount",
                                     "Coherence.Service.TaskAverageDuration",
                                     "Coherence.Service.TaskBacklog",
@@ -467,6 +468,7 @@ public class MetricsSupportTests
                                     "Coherence.Service.RequestPendingDuration",
                                     "Coherence.Service.RequestTimeoutCount",
                                     "Coherence.Service.RequestTotalCount",
+                                    "Coherence.Service.StatusHACode",
                                     "Coherence.Service.StorageEnabledCount",
                                     "Coherence.Service.TaskAverageDuration",
                                     "Coherence.Service.TaskBacklog",
