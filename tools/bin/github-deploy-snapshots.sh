edcb185e27692c90e625dcd1eea24523d1d52845
Chinmay Patel <chinmay.patel@oracle.com>
2022-02-24 21:24:49 +0000

    Update snapshot deploy script to check for snapshot version [ce/main -> ce/14.1.1 @ 90760] (auto-submit 90762 after successfully running remote remote.full) Job ID: job.9.20220224202601.10056
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 90763]

--- tools/bin/github-deploy-snapshots.sh
+++ tools/bin/github-deploy-snapshots.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 #
-# Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+# Copyright (c) 2000, 2022, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
 
 git checkout "${GIT_COMMIT}"
-REVISION_POM=prj/coherence-bom/pom.xml
+REVISION_POM=prj/pom.xml
 CURRENT_VERSION=$(grep -E "<revision>" ${REVISION_POM} | sed 's/.*<revision>\(.*\)<\/revision>/\1/')
 
 if [ "${CURRENT_VERSION}" = "" ]; then
   echo "Could not find current version from ${REVISION_POM}"
   exit 1
+fi
+
+if [ -z "$(echo $CURRENT_VERSION | grep SNAPSHOT)" ]; then
+  echo "This job only deploys SNAPSHOT versions, skipping version ${CURRENT_VERSION}"
+  exit 0
 fi
 
 echo "Building version ${CURRENT_VERSION}"
