a8f3aa6cfe264633225cf9c5cf1db34be946f45c
Ryan Lubke <ryan.lubke@oracle.com>
2022-03-23 23:07:50 +0000

    BUG 33993670 - [33911163->22.06] RFA: Terminating PartitionedCache due to unhandled exception: StackOverflowError (merge main -> ce/main @ 91164)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 91168]

--- prj/coherence-core/src/main/java/com/tangosol/io/pof/PofHelper.java
+++ prj/coherence-core/src/main/java/com/tangosol/io/pof/PofHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2020, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -1236,7 +1236,14 @@ public abstract class PofHelper
                 break;
 
             case T_CHAR_STRING:            // char-string
-                in.skipBytes(in.readPackedInt());
+                int cb = in.readPackedInt();
+
+                if (cb == V_REFERENCE_NULL)
+                    {
+                    break;
+                    }
+
+                in.skipBytes(cb);
                 break;
 
             case T_DATETIME:               // datetime
