9d6c1444eebd2e7ca5ad529589bc65cf3148432f
Gunnar Hillert <gunnar.hillert@oracle.com>
2022-03-24 17:10:06 +0000

    Improve tests and polish README of Views guide (main -> ce/main @ 91180)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 91183]

--- prj/examples/guides/124-views/src/test/java/com/oracle/coherence/guides/queries/ViewTests.java
+++ prj/examples/guides/124-views/src/test/java/com/oracle/coherence/guides/queries/ViewTests.java
@@ -6,11 +6,14 @@
  */
 package com.oracle.coherence.guides.queries;
 
+import com.oracle.bedrock.testsupport.deferred.Eventually;
+
 import com.oracle.coherence.guides.queries.model.Country;
 import com.oracle.coherence.guides.queries.utils.CoherenceHelper;
-import com.tangosol.net.Coherence;
 
+import com.tangosol.net.Coherence;
 import com.tangosol.net.NamedCache;
+
 import com.tangosol.net.Session;
 import com.tangosol.net.cache.ContinuousQueryCache;
 import com.tangosol.util.Filter;
@@ -33,7 +36,9 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 /**
  * A test class showing showing the usage of Coherence Views using ContinuousQueryCache.
@@ -71,12 +76,12 @@ class ViewTests {
 
         Set<Map.Entry<String, Country>> results = map.entrySet(filter); // <3>
 
-        assertThat(results).hasSize(2); // <4>
+        assertThat(results, hasSize(2)); // <4>
 
         Country mexico = new Country("Mexico", "Ciudad de México", 126.01); // <5>
         map.put("mx", mexico);
 
-        assertThat(results).hasSize(2); // <6>
+        assertThat(results, hasSize(2)); // <6>
 
     }
     // # end::testGreaterEqualsFilterWithChanges[]
@@ -90,12 +95,12 @@ class ViewTests {
 
         ContinuousQueryCache results = new ContinuousQueryCache(map, filter); // <3>
 
-        assertThat(results).hasSize(2); // <4>
+        assertThat(results.size(), is(2)); // <4>
 
         Country mexico = new Country("Mexico", "Ciudad de México", 126.01); // <5>
         map.put("mx", mexico);
 
-        assertThat(results).hasSize(3); // <6>
+        assertThat(results.size(), is(3)); // <6>
     }
     // # end::testGreaterEqualsFilterWithContinuousQueryCache[]
 
@@ -117,13 +122,13 @@ class ViewTests {
         results.addMapListener(listener);  // <3>
 
 
-        assertThat(results).hasSize(2); // <4>
+        assertThat(results.size(), is(2)); // <4>
 
         Country mexico = new Country("Mexico", "Ciudad de México", 126.01);  // <5>
         map.put("mx", mexico);
 
-        assertThat(results).hasSize(3); // <6>
-        assertThat(counter.get()).isEqualTo(1); // <7>
+        assertThat(results.size(), is(3)); // <6>
+        Eventually.assertDeferred(counter::get, is(1)); // <7>
     }
     // # end::testContinuousQueryCacheWithListener[]
 
@@ -137,24 +142,24 @@ class ViewTests {
         ContinuousQueryCache<String, Country, Double> results = new ContinuousQueryCache(map, filter, extractor);
 
         BigDecimalSum<BigDecimal> aggregator = new BigDecimalSum(new IdentityExtractor<>()); // <1>
-        AtomicReference<BigDecimal> aggregatedPopulation = new AtomicReference<>(formatNumber(results.aggregate(aggregator))); // <4>
+        AtomicReference<BigDecimal> aggregatedPopulation = new AtomicReference<>(formatNumber(results.aggregate(aggregator))); // <2>
 
-        MapListener<String, Double> listener = new SimpleMapListener<String, Double>()
+        MapListener<String, Double> listener = new SimpleMapListener<String, Double>()  // <3>
                 .addInsertHandler((event) -> {
                     aggregatedPopulation.set(formatNumber(results.aggregate(aggregator)));
                 });
-        results.addMapListener(listener);
+        results.addMapListener(listener); // <4>
 
-        assertThat(aggregatedPopulation.get()).isEqualTo(formatNumber(150.6)); // <8>
+        assertThat(aggregatedPopulation.get(), is(formatNumber(150.6))); // <5>
 
-        Country mexico = new Country("Mexico", "Ciudad de México", 126.01);
+        Country mexico = new Country("Mexico", "Ciudad de México", 126.01); // <6>
         map.put("mx", mexico);
 
-        assertThat(results).hasSize(3);
-        assertThat(aggregatedPopulation.get()).isEqualTo(formatNumber(276.61)); // <8>
+        assertThat(results.size(), is(3)); // <7>
+        Eventually.assertDeferred(aggregatedPopulation::get, is(formatNumber(276.61))); // <8>
 
     }
-    // # end::testAggregator[]
+    // # end::testAggregate[]
 
     private BigDecimal formatNumber(double input) {
         return formatNumber(BigDecimal.valueOf(input));
