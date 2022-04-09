88ed97a6916d370721d7e3c67b98e333ecb4f499
Ryan Lubke <ryan.lubke@oracle.com>
2022-04-09 18:31:08 +0000

    BUG 34007969 - Build: NettyRestTests>AbstractRestTests.testJsonPTKeyAndValue:1680->AbstractRestTests.testJsonPassThrough (merge main -> ce/main @ 91468)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 91476]

--- prj/test/functional/rest-netty/src/test/java/rest/NettyRestTests.java
+++ prj/test/functional/rest-netty/src/test/java/rest/NettyRestTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022 Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -85,7 +85,7 @@ public class NettyRestTests
         if (m_client == null)
             {
             m_client = createClient().build();
-            m_client.property(ClientProperties.READ_TIMEOUT, 2000);
+            m_client.property(ClientProperties.READ_TIMEOUT, 10000);
             }
 
         return m_client;
