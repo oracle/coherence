34e225b02075b15ae13f3c88ea565606b2fcd10f
luk.ho <luk.ho@oracle.com>
2022-04-13 19:47:18 +0000

    Bug 33695651 - Build: QuorumTests.testQuorumReject (main -> ce-main @90312)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 91583]

--- prj/coherence-core/src/main/java/com/oracle/coherence/common/internal/net/RunnableSelectionService.java
+++ prj/coherence-core/src/main/java/com/oracle/coherence/common/internal/net/RunnableSelectionService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
@@ -160,7 +160,7 @@ public class RunnableSelectionService
      *
      * @return the channel count.
      */
-    protected int getChannelCount()
+    protected synchronized int getChannelCount()
         {
         Selector selector = m_selector;
         return selector == null
