705610dd2bf4412c9521ed309ddcd37e992556ce
tmiddleton <tim.middleton@oracle.com>
2022-04-26 01:50:55 +0000

    Enh 34060978 - [34014993->14.1.1.0.10-CE] ENH: Add JMX report to show storage manager MBean information
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 92015]

--- prj/test/functional/reporter/src/test/java/reporter/ReportStorageTests.java
+++ prj/test/functional/reporter/src/test/java/reporter/ReportStorageTests.java
@@ -4,28 +4,26 @@
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * http://oss.oracle.com/licenses/upl.
  */
-package reporter;
 
+package reporter;
 
 import com.oracle.bedrock.testsupport.deferred.Eventually;
 
-import com.tangosol.net.CacheFactory;
-import com.tangosol.net.Cluster;
-import com.tangosol.net.NamedCache;
+import com.tangosol.coherence.reporter.ReportBatch;
 
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
 import com.tangosol.net.PartitionedService;
-import com.tangosol.net.management.MBeanServerProxy;
 
 import common.AbstractFunctionalTest;
 
-import javax.management.openmbean.TabularData;
+import org.hamcrest.Matchers;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static com.oracle.bedrock.deferred.DeferredHelper.invoking;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Tests to confirm storage manager report can be run.
@@ -44,24 +42,21 @@ public class ReportStorageTests
     public static void _startup()
         {
         System.setProperty("coherence.management", "all");
-        System.setProperty("coherence.distributed.localstorage", "false");
 
-        // disable until failure resolved
-        //AbstractFunctionalTest._startup();
+        AbstractFunctionalTest._startup();
         }
 
     // ----- test methods ---------------------------------------------------
 
-    // disable until failure resolved
-    // @Test
+    @Test
     public void testStorageManagerReport()
         {
         String sServer = "StorageMangerReport";
 
         try
             {
-            String sProjectName = "ReportStorage";
-            Cluster cluster = CacheFactory.ensureCluster();
+            String      sProjectName = "ReportStorage";
+            ReportBatch batch        = new ReportBatch();
 
             startCacheServer(sServer + "-1", sProjectName);
             startCacheServer(sServer + "-2", sProjectName);
@@ -69,17 +64,11 @@ public class ReportStorageTests
             NamedCache<String, String> cache    = CacheFactory.getCache("test");
             PartitionedService         service  = (PartitionedService) cache.getCacheService();
 
-            Eventually.assertThat(invoking(service).getOwnershipEnabledMembers().size(), is(2));
-
             cache.put("key", "value");
 
-            MBeanServerProxy proxy = cluster.getManagement().getMBeanServerProxy();
-            TabularData reportData = (TabularData) proxy.invoke("Coherence:type=Reporter,nodeId=1", "runTabularReport",
-                    new Object[]{"reports/report-cache-storage.xml"}, new String[]{"java.lang.String"});
+            Eventually.assertThat(invoking(service).getOwnershipEnabledMembers().size(), is(2));
 
-            // this is a good enough test to make sure we get two rows in the reportData meaning
-            // two StorageManagerMBean results for the created cache
-            assertEquals(2, reportData.size());
+            Eventually.assertDeferred(batch.runTabularReport(REPORT).toString(), () -> batch.runTabularReport(REPORT).keySet().size(), Matchers.is(2));
             }
         catch (Exception e)
             {
@@ -92,4 +81,6 @@ public class ReportStorageTests
             stopCacheServer(sServer + "-2");
             }
         }
+
+    private static final String REPORT = "reports/report-cache-storage.xml";
     }
