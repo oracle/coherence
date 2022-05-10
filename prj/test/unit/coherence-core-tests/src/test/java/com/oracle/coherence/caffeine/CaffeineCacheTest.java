5a8e5b7b3f1859196fdaec5ea78285c7b8b7fa2e
Aleks Seovic <aleks@seovic.com>
2022-05-10 03:17:34 -0400

    Rebased to latest main and moved Caffeine integration into coherence-core.

--- prj/test/unit/coherence-core-tests/src/test/java/com/oracle/coherence/caffeine/CaffeineCacheTest.java
+++ prj/test/unit/coherence-core-tests/src/test/java/com/oracle/coherence/caffeine/CaffeineCacheTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package com.oracle.coherence.caffeine;
