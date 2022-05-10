60c4d00780196d8a81f3f455076d8e1f414fa5f0
luk.ho <luk.ho@oracle.com>
2022-05-10 14:06:43 +0000

    Bug 32224432 - Build: clusteredContainerGARRuntime-rolling-restart.testRollingRestartCluster failed due to NullPointerException (12.2.1.4 -> ce-14.1.1.0 @92042)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 92463]

--- prj/coherence-core/src/main/java/com/tangosol/internal/metrics/MetricSupport.java
+++ prj/coherence-core/src/main/java/com/tangosol/internal/metrics/MetricSupport.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package com.tangosol.internal.metrics;
 
@@ -275,7 +275,14 @@ public class MetricSupport
 
         f_setRegistered.add(sMBeanName);
 
-        MBeanInfo        mBeanInfo = proxy.getMBeanInfo(sMBeanName);
+        MBeanInfo mBeanInfo = proxy.getMBeanInfo(sMBeanName);
+        if (mBeanInfo == null)
+            {
+            CacheFactory.log("MetricSupport.registerInternal(), mBeanInfo is null on MBean: " + sMBeanName
+                    + ", proxy: " + proxy, Base.LOG_WARN);
+            return;
+            }
+
         Set<MBeanMetric> setMetric = getMetrics(sMBeanName, mBeanInfo, proxy);
         if (setMetric.size() > 0)
             {
