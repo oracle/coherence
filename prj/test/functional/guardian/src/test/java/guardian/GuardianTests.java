21d7d939a7e5b1f80b725857d1da1557f836b2c7
Chinmay Patel <chinmay.patel@oracle.com>
2022-05-13 13:43:31 +0000

    Null integ (14.1.1 -> ce/14.1.1 @ 89992)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 92616]

--- prj/test/functional/guardian/src/test/java/guardian/GuardianTests.java
+++ prj/test/functional/guardian/src/test/java/guardian/GuardianTests.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package guardian;
-
 
 import com.tangosol.net.CacheFactory;
 import com.tangosol.net.CacheService;
