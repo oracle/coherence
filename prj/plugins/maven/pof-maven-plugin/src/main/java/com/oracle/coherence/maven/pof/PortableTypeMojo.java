4ba653e8615f5d68ad61a1ae55b9e8fe3a21adba
Jonathan Knight <jonathan.knight@oracle.com>
2022-06-09 15:52:08 +0000

    COH-25658 Fix code coverage build (merge 14.1.1.2206 -> ce/22.06 93338)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 93339]

--- prj/plugins/maven/pof-maven-plugin/src/main/java/com/oracle/coherence/maven/pof/PortableTypeMojo.java
+++ prj/plugins/maven/pof-maven-plugin/src/main/java/com/oracle/coherence/maven/pof/PortableTypeMojo.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2020 Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package com.oracle.coherence.maven.pof;
 
@@ -79,14 +79,16 @@ public abstract class PortableTypeMojo
     public void execute()
             throws MojoExecutionException
         {
-        if (m_fSkip)
-            {
-            return;
-            }
-
         try
             {
-            MavenLogger           log            = new MavenLogger(getLog());
+            MavenLogger log = new MavenLogger(getLog());
+
+            if (m_fSkip)
+                {
+                log.info("PortableTypeGenerator code generation is skipped");
+                return;
+                }
+            
             SchemaBuilder         builder        = new SchemaBuilder();
             ClassFileSchemaSource source         = new ClassFileSchemaSource();
             File[]                aDirs          = getClassesDirectories();
