aa71c7be38a6c2b59473c7e4d14273a46d97cfb0
luk.ho <luk.ho@oracle.com>
2022-09-20 20:24:35 +0000

    Bug 34619637 - SingleBufferMessageEvent may throw NPE when the source is NULL during cluster startup/shutdown. (14.1.1.0 -> ce-14.1.1.0 @95613)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 95619]

--- prj/coherence-core/src/main/java/com/oracle/coherence/common/internal/net/socketbus/SingleBufferMessageEvent.java
+++ prj/coherence-core/src/main/java/com/oracle/coherence/common/internal/net/socketbus/SingleBufferMessageEvent.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package com.oracle.coherence.common.internal.net.socketbus;
 
@@ -75,7 +75,7 @@ public class SingleBufferMessageEvent
     @Override
     public EndPoint getEndPoint()
         {
-        return m_src.getPeer();
+        return m_src == null ? null : m_src.getPeer();
         }
 
     /**
