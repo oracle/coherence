3e90d91bc35c2b8faf7572487cf8ea366df45967
joe.fialli <joe.fialli@oracle.com>
2022-10-04 23:41:18 +0000

    Bug 34664583 - [34655886->23.03] NPE in releaseBackingMap when invalidating ReplicatedCache  (main->ce-main @ 95933) (auto-submit 95879 after successfully running remote remote.full) Job ID: job.9.20221003153026.30203 (auto-submit integ 95933 main -> coherence-ce/main after successfully running remote remote.full) Job ID: job.9.20221004162746.24372 (auto-submit integ 95972 coherence-ce/main -> coherence-ce/release/coherence-ce-v22.09 after successfully running remote remote.full) Job ID: job.9.20221004212656.30659 (auto-submit integ 95983 coherence-ce/release/coherence-ce-v22.09 -> coherence-ce/release/coherence-ce-v22.06 after successfully running remote remote.full) Job ID: job.9.20221004223433.7075
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 95986]

--- prj/coherence-core/src/main/java/com/tangosol/net/ExtensibleConfigurableCacheFactory.java
+++ prj/coherence-core/src/main/java/com/tangosol/net/ExtensibleConfigurableCacheFactory.java
@@ -1925,8 +1925,12 @@ public class ExtensibleConfigurableCacheFactory
         @Override
         public void releaseBackingMap(String sName, Map map)
             {
-            MBeanHelper.unregisterCacheMBean(getContext().getCacheService(), sName, "tier=back");
+            BackingMapManagerContext context = getContext();
 
+            if (context != null)
+                {
+                MBeanHelper.unregisterCacheMBean(context.getCacheService(), sName, "tier=back");
+                }
             getCacheFactory().release(map, m_mapBackingMapListeners);
 
             setBackingMap(sName, null);
