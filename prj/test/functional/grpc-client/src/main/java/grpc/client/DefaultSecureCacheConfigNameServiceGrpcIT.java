5d0f26a60ef19479bea7afab5c3e398f653c6665
Jonathan Knight <jonathan.knight@oracle.com>
2022-11-03 10:45:26 +0000

    COH-26618 Build: Test failure in AbstractGrpcClientIT.shouldReceiveTruncateAndDeactivationEvent - adding debug logging to test (merge 14.1.1.2206 -> ce/22.06 96396)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 96397]

--- prj/test/functional/grpc-client/src/main/java/grpc/client/DefaultSecureCacheConfigNameServiceGrpcIT.java
+++ prj/test/functional/grpc-client/src/main/java/grpc/client/DefaultSecureCacheConfigNameServiceGrpcIT.java
@@ -35,7 +35,10 @@ import com.tangosol.net.Session;
 import com.tangosol.net.SessionConfiguration;
 import com.tangosol.net.grpc.GrpcDependencies;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.util.HashMap;
@@ -114,6 +117,36 @@ public class DefaultSecureCacheConfigNameServiceGrpcIT
     static void shutdownCoherence()
         {
         Coherence.closeAll();
+        }
+
+    @BeforeEach
+    public void logStart(TestInfo info)
+        {
+        String sMsg = ">>>>> Starting test: " + info.getDisplayName();
+        for (CoherenceClusterMember member : CLUSTER_EXTENSION.getCluster())
+            {
+            member.submit(() ->
+                {
+                System.err.println(sMsg);
+                System.err.flush();
+                return null;
+                }).join();
+            }
+        }
+
+    @AfterEach
+    public void logEnd(TestInfo info)
+        {
+        String sMsg = ">>>>> Finished test: " + info.getDisplayName();
+        for (CoherenceClusterMember member : CLUSTER_EXTENSION.getCluster())
+            {
+            member.submit(() ->
+                {
+                System.err.println(sMsg);
+                System.err.flush();
+                return null;
+                }).join();
+            }
         }
 
     @Override
