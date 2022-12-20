56a6724a6408720c1998d8a8fff8e2a4a223e14f
Chinmay Patel <chinmay.patel@oracle.com>
2022-12-20 22:28:47 +0000

    integ CL# 97366 (14.1.1 -> ce/14.1.1)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 97369]

--- prj/test/functional/aggregator/src/test/java/aggregator/async/PriorityAggregatorTests.java
+++ prj/test/functional/aggregator/src/test/java/aggregator/async/PriorityAggregatorTests.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package aggregator.async;
@@ -92,7 +92,7 @@ public class PriorityAggregatorTests
          Trade.fillRandom(cache, 100);
 
          PriorityAggregator priorityAggregator = new PriorityAggregator(new SlowAggregator("getPrice"));
-         priorityAggregator.setRequestTimeoutMillis(500L);
+         priorityAggregator.setRequestTimeoutMillis(1000L);
          try
              {
              cache.aggregate((Filter) null, priorityAggregator);
