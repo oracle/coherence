22d2dcfe6ee5a4af3092a0e95004c5147aae6977
Jonathan Knight <jonathan.knight@oracle.com>
2023-01-16 11:59:21 +0000

    BUG 34978547 - [34852376->22.06.4] Topic MBean should be registered on all cluster members (merge 14.1.1.2206 -> ce/22.06 97788) RQ job.9.20230116103330.13277
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 97791]

--- prj/test/functional/topics/src/main/java/topics/PagedTopicMBeanTests.java
+++ prj/test/functional/topics/src/main/java/topics/PagedTopicMBeanTests.java
@@ -1,15 +1,27 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
  */
 package topics;
 
+import com.oracle.bedrock.junit.CoherenceClusterResource;
+import com.oracle.bedrock.runtime.coherence.CoherenceClusterMember;
+import com.oracle.bedrock.runtime.coherence.JMXManagementMode;
+import com.oracle.bedrock.runtime.coherence.options.ClusterName;
+import com.oracle.bedrock.runtime.coherence.options.LocalHost;
+import com.oracle.bedrock.runtime.coherence.options.LocalStorage;
+import com.oracle.bedrock.runtime.coherence.options.RoleName;
+import com.oracle.bedrock.runtime.coherence.options.WellKnownAddress;
+import com.oracle.bedrock.runtime.java.options.IPv4Preferred;
+import com.oracle.bedrock.runtime.options.DisplayName;
 import com.oracle.bedrock.testsupport.deferred.Eventually;
+import com.oracle.bedrock.testsupport.junit.TestLogs;
 import com.tangosol.internal.net.topic.impl.paged.PagedTopicSubscriber;
 import com.tangosol.internal.net.topic.impl.paged.model.SubscriberGroupId;
 import com.tangosol.net.Coherence;
+import com.tangosol.net.Member;
 import com.tangosol.net.Session;
 import com.tangosol.net.TopicService;
 import com.tangosol.net.management.MBeanHelper;
@@ -19,6 +31,7 @@ import com.tangosol.net.topic.NamedTopic;
 import com.tangosol.util.Filters;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -31,6 +44,10 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.collection.IsEmptyIterable.emptyIterable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SuppressWarnings({"unchecked", "unused"})
 public class PagedTopicMBeanTests
@@ -39,10 +56,12 @@ public class PagedTopicMBeanTests
     @SuppressWarnings("resource")
     public static void setup() throws Exception
         {
-        System.setProperty("coherence.cluster", "PagedTopicMBeanTests");
+        System.setProperty("coherence.cluster", CLUSTER_NAME);
         System.setProperty("coherence.wka", "127.0.0.1");
         System.setProperty("coherence.localhost", "127.0.0.1");
         System.setProperty("coherence.ttl", "0");
+        System.setProperty(JMXManagementMode.PROPERTY, "all");
+        System.setProperty(LocalStorage.PROPERTY, "false");
 
         s_coherence  = Coherence.clusterMember().start().get(5, TimeUnit.MINUTES);
         s_session    = s_coherence.getSession();
@@ -63,16 +82,49 @@ public class PagedTopicMBeanTests
 
         try (NamedTopic<String> topic = s_session.getTopic(sTopicName))
             {
-            String sMBeanName = s_registry.ensureGlobalName(MBeanHelper.getTopicMBeanName(topic));
-            assertThat(s_mBeanProxy.getMBeanInfo(sMBeanName), is(notNullValue()));
+            // MBean should not be on this member (storage disabled)
+            String sMBeanNamePrefix = MBeanHelper.getTopicMBeanName(topic);
+            String sLocalMBeanName  = s_registry.ensureGlobalName(sMBeanNamePrefix);
+            assertThat(s_mBeanProxy.getMBeanInfo(sLocalMBeanName), is(nullValue()));
+
+            // MBean should be on storage enabled members
+            String      sQuery   = sMBeanNamePrefix + ",*";
+            Set<String> setNames = s_mBeanProxy.queryNames(sMBeanNamePrefix + ",*", Filters.always());
+            String      sDomain  = s_registry.getDomainName() + ":";
+
+            for (CoherenceClusterMember clusterMember : s_cluster.getCluster().getAll("Storage"))
+                {
+                Member member = mock(Member.class);
+                when(member.getId()).thenReturn(clusterMember.getLocalMemberId());
+
+                String sMBeanName = sDomain + s_registry.ensureGlobalName(sMBeanNamePrefix, member);
+                Eventually.assertDeferred("MBean should be on member " + clusterMember.getName(),
+                                          () -> s_mBeanProxy.getMBeanInfo(sMBeanName), is(notNullValue()));
+                }
+
+            // MBean should NOT be on storage disabled members
+            for (CoherenceClusterMember clusterMember : s_cluster.getCluster().getAll("NonStorage"))
+                {
+                Member member = mock(Member.class);
+                when(member.getId()).thenReturn(clusterMember.getLocalMemberId());
+
+                String sMBeanName = s_registry.ensureGlobalName(sMBeanNamePrefix, member);
+                Eventually.assertDeferred("MBean should not be on member " + clusterMember.getName(),
+                                          () -> s_mBeanProxy.getMBeanInfo(sMBeanName), is(nullValue()));
+                }
 
             topic.destroy();
-            assertThat(s_mBeanProxy.getMBeanInfo(sMBeanName), is(nullValue()));
+
+            // MBean should not be on this member
+            assertThat(s_mBeanProxy.getMBeanInfo(sLocalMBeanName), is(nullValue()));
+
+            // MBean should not be on any other cluster member
+            Eventually.assertDeferred(() -> s_mBeanProxy.queryNames(sQuery, Filters.always()), is(emptyIterable()));
             }
         }
 
     @Test
-    public void shouldRegisterAndUnregisterGroups()
+    public void shouldRegisterAndUnregisterGroups()  throws Exception
         {
         SubscriberGroupId  groupOneId = SubscriberGroupId.withName("group-one");
         SubscriberGroupId  groupTwoId = SubscriberGroupId.withName("group-two");
@@ -85,19 +137,80 @@ public class PagedTopicMBeanTests
             String       sNameTwo = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(groupTwoId, sTopicName, service));
 
             topic.ensureSubscriberGroup(groupOneId.getGroupName());
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(notNullValue()));
+            // GroupOne MBean should not be on this member
+            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
+            // GroupOne MBean should be on storage members
+            assertSubscriberGroupPresent(groupOneId, sTopicName, service);
+            // GroupOne MBean should not be on non-storage members
+            assertSubscriberGroupNotPresent(groupOneId, sTopicName, service);
 
             topic.ensureSubscriberGroup(groupTwoId.getGroupName());
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(notNullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(notNullValue()));
+            // GroupTwo MBean should not be on this member
+            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(nullValue()));
+            // GroupTwo MBean should be on storage members
+            assertSubscriberGroupPresent(groupTwoId, sTopicName, service);
+            // GroupTwo MBean should not be on non-storage members
+            assertSubscriberGroupNotPresent(groupTwoId, sTopicName, service);
+            // GroupOne MBean should not be on this member
+            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
+            // GroupOne MBean should be on storage members
+            assertSubscriberGroupPresent(groupOneId, sTopicName, service);
+            // GroupOne MBean should not be on non-storage members
+            assertSubscriberGroupNotPresent(groupOneId, sTopicName, service);
 
             topic.destroySubscriberGroup(groupOneId.getGroupName());
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(notNullValue()));
+            // GroupOne MBean should be gone
+            String      sQuery   = sNameOne + ",*";
+            Set<String> setNames = s_mBeanProxy.queryNames(sQuery, Filters.always());
+            assertThat(setNames, is(emptyIterable()));
+            // GroupTwo MBean should not be on this member
+            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(nullValue()));
+            // GroupTwo MBean should be on storage members
+            assertSubscriberGroupPresent(groupTwoId, sTopicName, service);
+            // GroupTwo MBean should not be on non-storage members
+            assertSubscriberGroupNotPresent(groupTwoId, sTopicName, service);
 
             topic.destroySubscriberGroup(groupTwoId.getGroupName());
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(nullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
+            // GroupOne MBean should be gone
+            sQuery   = sNameOne + ",*";
+            setNames = s_mBeanProxy.queryNames(sQuery, Filters.always());
+            assertThat(setNames, is(emptyIterable()));
+            // GroupTwo MBean should be gone
+            sQuery   = sNameOne + ",*";
+            setNames = s_mBeanProxy.queryNames(sQuery, Filters.always());
+            assertThat(setNames, is(emptyIterable()));
+            }
+        }
+
+    private void assertSubscriberGroupPresent(SubscriberGroupId groupId, String sTopicName, TopicService service)
+        {
+        String sMBeanNamePrefix = MBeanHelper.getSubscriberGroupMBeanName(groupId, sTopicName, service);
+        String sDomain          = s_registry.getDomainName() + ":";
+
+        for (CoherenceClusterMember clusterMember : s_cluster.getCluster().getAll("Storage"))
+            {
+            Member member = mock(Member.class);
+            when(member.getId()).thenReturn(clusterMember.getLocalMemberId());
+
+            String sMBeanName = sDomain + s_registry.ensureGlobalName(sMBeanNamePrefix, member);
+            Eventually.assertDeferred("MBean should be on member " + clusterMember.getName(),
+                                      () -> s_mBeanProxy.getMBeanInfo(sMBeanName), is(notNullValue()));
+            }
+        }
+
+    private void assertSubscriberGroupNotPresent(SubscriberGroupId groupId, String sTopicName, TopicService service)
+        {
+        String sMBeanNamePrefix = MBeanHelper.getSubscriberGroupMBeanName(groupId, sTopicName, service);
+        String sDomain          = s_registry.getDomainName() + ":";
+
+        for (CoherenceClusterMember clusterMember : s_cluster.getCluster().getAll("NonStorage"))
+            {
+            Member member = mock(Member.class);
+            when(member.getId()).thenReturn(clusterMember.getLocalMemberId());
+
+            String sMBeanName = sDomain + s_registry.ensureGlobalName(sMBeanNamePrefix, member);
+            Eventually.assertDeferred("MBean should not be on member " + clusterMember.getName(),
+                                      () -> s_mBeanProxy.getMBeanInfo(sMBeanName), is(nullValue()));
             }
         }
 
@@ -115,15 +228,18 @@ public class PagedTopicMBeanTests
             topic.ensureSubscriberGroup(groupOneId.getGroupName());
             topic.ensureSubscriberGroup(groupTwoId.getGroupName());
 
-            String sNameOne = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(groupOneId, sTopicName, service));
-            String sNameTwo = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(groupTwoId, sTopicName, service));
+            String sNameOne  = MBeanHelper.getSubscriberGroupMBeanName(groupOneId, sTopicName, service);
+            String sNameTwo  = MBeanHelper.getSubscriberGroupMBeanName(groupTwoId, sTopicName, service);
+            String sQueryOne = sNameOne + ",*";
+            String sQueryTwo = sNameTwo + ",*";
 
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(notNullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(notNullValue()));
+            assertThat(s_mBeanProxy.queryNames(sQueryOne, Filters.always()), is(not(emptyIterable())));
+            assertThat(s_mBeanProxy.queryNames(sQueryTwo, Filters.always()), is(not(emptyIterable())));
 
             topic.destroy();
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(nullValue()));
+
+            assertThat(s_mBeanProxy.queryNames(sQueryOne, Filters.always()), is(emptyIterable()));
+            assertThat(s_mBeanProxy.queryNames(sQueryTwo, Filters.always()), is(emptyIterable()));
             }
         }
 
@@ -227,39 +343,33 @@ public class PagedTopicMBeanTests
         }
 
     @Test
-    public void shouldRegisterGroupsForDurableSubscribers()
+    public void shouldRegisterGroupsForDurableSubscribers() throws Exception
         {
         String sTopicName = m_testWatcher.getMethodName();
-        String sNameOne;
-        String sNameTwo;
+        String sDomain    = s_registry.getDomainName();
 
         try (NamedTopic<String> topic = s_session.getTopic(sTopicName))
             {
+            TopicService service       = topic.getTopicService();
+            String       sGroupPattern = sDomain + ":" + MBeanHelper.getSubscriberGroupMBeanName(null, sTopicName, service) + ",*";
+
             try (PagedTopicSubscriber<String> subscriberOne   = (PagedTopicSubscriber<String>) topic.createSubscriber(inGroup("one"));
                  PagedTopicSubscriber<String> subscriberTwo   = (PagedTopicSubscriber<String>) topic.createSubscriber(inGroup("one"));
                  PagedTopicSubscriber<String> subscriberThree = (PagedTopicSubscriber<String>) topic.createSubscriber(inGroup("two")))
                 {
-                TopicService service       = topic.getTopicService();
-                String       sGroupPattern = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(null, sTopicName, service));
-                Set<String>  setNames      = s_mBeanProxy.queryNames(sGroupPattern, Filters.always());
-
-                assertThat(setNames.size(), is(2));
-
-                sNameOne = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(subscriberOne.getSubscriberGroupId(), sTopicName, service));
-                sNameTwo = s_registry.ensureGlobalName(MBeanHelper.getSubscriberGroupMBeanName(subscriberThree.getSubscriberGroupId(), sTopicName, service));
-
-                assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(notNullValue()));
-                assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(notNullValue()));
+                // Group Mbeans should be on each storage member
+                assertThat(s_mBeanProxy.queryNames(sGroupPattern, Filters.always()).size(), is(STORAGE_ENABLED_COUNT * 2));
                 }
 
             // durable groups should still exist after subscribers have closed
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(notNullValue()));
-            assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(notNullValue()));
+            // Group Mbeans should be on each storage member
+            assertThat(s_mBeanProxy.queryNames(sGroupPattern, Filters.always()).size(), is(STORAGE_ENABLED_COUNT * 2));
             }
-        // durable groups should not exist after the topic has closed
-        assertThat(s_mBeanProxy.getMBeanInfo(sNameOne), is(nullValue()));
-        assertThat(s_mBeanProxy.getMBeanInfo(sNameTwo), is(nullValue()));
         }
+
+    // ----- constants ------------------------------------------------------
+
+    public static final String CLUSTER_NAME = "PagedTopicMBeanTests";
 
     // ----- data members ---------------------------------------------------
 
@@ -273,4 +383,26 @@ public class PagedTopicMBeanTests
 
     @Rule(order = 1)
     public TestName m_testWatcher = new TestName();
+
+    public static final int STORAGE_ENABLED_COUNT = 3;
+
+    @ClassRule(order = 1)
+    public static final TestLogs s_tetLogs = new TestLogs(PagedTopicMBeanTests.class);
+
+    @ClassRule(order = 2)
+    public static final CoherenceClusterResource s_cluster = new CoherenceClusterResource()
+            .with(WellKnownAddress.loopback(),
+                     JMXManagementMode.ALL,
+                     LocalHost.only(),
+                     ClusterName.of(CLUSTER_NAME),
+                     IPv4Preferred.yes(),
+                     s_tetLogs)
+            .include(STORAGE_ENABLED_COUNT, CoherenceClusterMember.class,
+                     LocalStorage.enabled(),
+                     RoleName.of("storage"),
+                     DisplayName.of("Storage"))
+            .include(3, CoherenceClusterMember.class,
+                     LocalStorage.disabled(),
+                     RoleName.of("non-storage"),
+                     DisplayName.of("NonStorage"));
     }
