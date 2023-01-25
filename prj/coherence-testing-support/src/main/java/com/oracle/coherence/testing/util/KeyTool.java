369b638df629b05636fbb6c8bd50c50b7a101a1d
Jonathan Knight <jonathan.knight@oracle.com>
2023-01-25 16:31:12 +0000

    BUG 34820911 - Build: Fix the SSL builds to run on the latest Ubuntu on GitHub (merge 14.1.1.2206 -> ce/22.06 98050)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 98051]

--- prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/util/KeyTool.java
+++ prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/util/KeyTool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -236,7 +236,7 @@ public class KeyTool
 
     private static void toPem(File fileKey, File filePEM, String sKeyPass)
         {
-        Arguments arguments = Arguments.of("pkcs8", "-topk8", "-outform", "pem", "-v1", "PBE-MD5-DES",
+        Arguments arguments = Arguments.of("pkcs8", "-topk8", "-outform", "pem", "-v1", "PBE-SHA1-3DES",
                                            "-in", fileKey.getAbsolutePath(),
                                            "-out", filePEM.getAbsolutePath());
 
