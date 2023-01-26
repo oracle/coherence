8b771e3ab36e96a07e43039e8854b17f07ae04f9
joe.fialli <joe.fialli@oracle.com>
2023-01-26 20:34:01 +0000

    COH-26709 Build: test/functional/ssl is expotentially longer to run on linux systems (auto-submit 97967 after successfully running remote remote.function) Job ID: job.9.20230126200027.12190 (auto-submit integ 98101 main -> coherence-ce/main after successfully running remote remote.function) Job ID: job.9.20230126202100.13968
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 98103]

--- prj/test/functional/ssl/src/main/java/ssl/BaseSocketProviderTests.java
+++ prj/test/functional/ssl/src/main/java/ssl/BaseSocketProviderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -167,7 +167,7 @@ public abstract class BaseSocketProviderTests
 
     protected int getIterations()
         {
-        return 1000;
+        return 100;
         }
 
     protected void trustedServerConfigTest(EchoClient client, EchoServer server)
