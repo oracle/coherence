3e9c27e069e5ea41cc77d28a93ff0183e60e266f
Ryan Lubke <ryan.lubke@oracle.com>
2023-01-27 00:50:03 +0000

    Bug 35019703 - [34997645->14.1.1.0.13] Exception observed from LongArray during index request: java.lang.ArrayIndexOutOfBoundsException: Index 2 out of bounds for length 2 (merge 14.1.1 -> ce/14.1.1 @ 98131)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 98132]

--- prj/coherence-core/src/main/java/com/tangosol/util/LongArray.java
+++ prj/coherence-core/src/main/java/com/tangosol/util/LongArray.java
@@ -1,14 +1,19 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package com.tangosol.util;
 
 
 import java.io.Serializable;
+
+import java.util.Spliterators;
+
+import java.util.stream.StreamSupport;
+
 
 /**
 * An interface, similar in its methods to List, and similar in its purpose
@@ -274,14 +279,10 @@ public interface LongArray<V>
      */
     public default long[] keys()
         {
-        long[] alKeys = new long[getSize()];
-        int    i      = 0;
-        for (Iterator<V> iter = iterator(); iter.hasNext(); )
-            {
-            iter.next();
-            alKeys[i++] = iter.getIndex();
-            }
-        return alKeys;
+        Iterator<V> iterator = iterator();
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, 0), false)
+                .mapToLong(o -> iterator.getIndex())
+                .toArray();
         }
 
 
