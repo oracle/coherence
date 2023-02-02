5191bbe6c920a24944adf5e287c18a403e48cc22
patrick.fry <patrick.fry@oracle.com>
2023-02-02 06:10:41 +0000

    BUG 35037453 - CreateKeyStore fails to compile due to https://bugs.openjdk.org/browse/JDK-8242151 RQ: job.9.20230201201239.29656 (auto-submit integ 98290 release/coherence-v14.1.1.0 -> coherence-ce/release/coherence-ce-v14.1.1.0 after successfully running remote remote.full) Job ID: job.9.20230202052614.15123
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 98294]

--- prj/test/functional/security/src/test/java/security/CreateKeyStore.java
+++ prj/test/functional/security/src/test/java/security/CreateKeyStore.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package security;
 
@@ -43,6 +43,10 @@ import java.util.Enumeration;
 
 /**
  * This class creates a set of keystores used by the Security tests.
+ *
+ * Note: This class no longer compiles due to
+ *       https://bugs.openjdk.org/browse/JDK-8242151
+ *       and is excluded in the <maven-compiler-plugin> configuration.
  *
  * @author lh 2018.1l.01
  */
