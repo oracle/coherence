5e7d3a50e946bdcd3cdd919df1fadf55b31c279d
luk.ho <luk.ho@oracle.com>
2023-04-07 20:09:45 +0000

    Bug 35219439 - Build: aggregator.async.DistMultiEntryAggregatorTests.* (main -> ce-main @99730)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 99756]

--- prj/coherence-core/src/main/java/com/tangosol/io/DecoratedBinaryDeltaCompressor.java
+++ prj/coherence-core/src/main/java/com/tangosol/io/DecoratedBinaryDeltaCompressor.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package com.tangosol.io;
@@ -394,6 +394,10 @@ public class DecoratedBinaryDeltaCompressor
         else
             {
             bufOld = getUndecorated(bufOld);
+            if (bufOld.length() == 0 && abufDeco.length > 1)
+                {
+                bufOld = AbstractReadBuffer.NO_BINARY;
+                }
             }
 
         // apply the value delta (if there is one)
