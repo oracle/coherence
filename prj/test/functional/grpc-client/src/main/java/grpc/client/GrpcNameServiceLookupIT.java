b5bdf6594f63d399b788ddbb003478de11ae5e7d
Jonathan Knight <jonathan.knight@oracle.com>
2023-06-07 11:04:56 +0000

    Fix intermittent failure in GrpcNameServiceLookupIT (merge 14.1.1.2206 -> ce/22.06 100810)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 100811]

--- prj/test/functional/grpc-client/src/main/java/grpc/client/GrpcNameServiceLookupIT.java
+++ prj/test/functional/grpc-client/src/main/java/grpc/client/GrpcNameServiceLookupIT.java
@@ -21,6 +21,7 @@ import com.oracle.bedrock.runtime.java.options.SystemProperty;
 import com.oracle.bedrock.runtime.network.AvailablePortIterator;
 import com.oracle.bedrock.runtime.options.DisplayName;
 import com.oracle.bedrock.runtime.options.Ports;
+import com.oracle.bedrock.testsupport.deferred.Eventually;
 import com.oracle.bedrock.testsupport.junit.TestLogsExtension;
 import com.oracle.coherence.client.GrpcRemoteCacheService;
 import com.tangosol.coherence.component.util.safeService.SafeCacheService;
@@ -49,7 +50,10 @@ public class GrpcNameServiceLookupIT
     static void setup() throws Exception
         {
         CoherenceClusterMember clusterMember = CLUSTER_EXTENSION.getCluster().getAny();
-        int                    grpcPort      = clusterMember.getGrpcProxyPort();
+
+        Eventually.assertDeferred(() -> clusterMember.isServiceRunning(GrpcDependencies.SCOPED_PROXY_SERVICE_NAME), is(true));
+
+        int grpcPort = clusterMember.getGrpcProxyPort();
 
         System.setProperty("coherence.wka", "127.0.0.1");
         System.setProperty("coherence.override", "test-coherence-override.xml");
