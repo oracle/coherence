8789a7f10066a96ead14d07043efa98aab017101
patrick.fry <patrick.fry@oracle.com>
2023-06-14 17:50:52 +0000

    COH-27815 - Build: PartitionedCacheTests.testNonObservableBackingMap (14.1.1.0 cl 100572 --> ce/14.1.1.0)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 101073]

--- prj/test/functional/partition/src/test/java/partition/PartitionedCacheTests.java
+++ prj/test/functional/partition/src/test/java/partition/PartitionedCacheTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -523,7 +523,7 @@ public class PartitionedCacheTests
     @Test
     public void testNonObservableBackingMap()
         {
-        final int CACHE_SIZE = 200;
+        final int CACHE_SIZE = 200000;
         NamedCache cache = getNamedCache("custom-backing-map");
         try
             {
