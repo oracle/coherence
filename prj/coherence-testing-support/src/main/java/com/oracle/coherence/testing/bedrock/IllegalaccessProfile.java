16b0526cfdd67aa7378d299f28bc3bb91f4cb30e
joe.fialli <joe.fialli@oracle.com>
2023-07-30 23:16:48 +0000

    Enh 35656189 - [35401756->23.09] Remove requirement to --add-opens java.base/java.lang.invoke=com.oracle.coherence to JDK 11/17 or higher startup  (main -> ce-main @ 102172) Remote remote.full on coherence-ce/main success, changes 102186, synced @102187, job.9.20230730210643.25905
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 102188]

--- prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/bedrock/IllegalaccessProfile.java
+++ prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/bedrock/IllegalaccessProfile.java
@@ -58,7 +58,6 @@ public class IllegalaccessProfile
                         "--add-opens=java.base/java.nio=ALL-UNNAMED",
                         "--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED",
                         "--add-exports=java.management/sun.management=ALL-UNNAMED",
-                        "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED",
                         "--add-opens=java.base/java.lang=ALL-UNNAMED");
 
             // Support was removed in JDK 17 so avoid warning message by not including in JDK 17 and greater
