dba2ad53083ee37dc7d57933bb96e5cb920449b8
Ryan Lubke <ryan.lubke@oracle.com>
2023-08-31 02:09:54 +0000

    BUG 35760966 - [build] buildah images for 1.30 and 1.29 are not available (merge 14.1.1 -> ce/14.1.1 @ 103006)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 103007]

--- prj/coherence-docker/run-buildah.sh
+++ prj/coherence-docker/run-buildah.sh
@@ -110,6 +110,6 @@ else
       -e HTTP_PROXY="${HTTP_PROXY}" -e HTTPS_PROXY="${HTTPS_PROXY}" -e NO_PROXY="${NO_PROXY}" \
       -e http_proxy="${http_proxy}" -e https_proxy="${https_proxy}" -e no_proxy="${no_proxy}" \
       --name buildah \
-      quay.io/buildah/stable:v1.29.0 "${SCRIPT_NAME}"
+      quay.io/buildah/stable:v1.31.0 "${SCRIPT_NAME}"
 fi
 
