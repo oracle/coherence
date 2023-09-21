4daee9eaa9bb1be803b9bdc2a6a7c45a09de3384
tmiddleton <tim.middleton@oracle.com>
2023-09-21 02:25:39 +0000

    Examples: remove unused imports (ce-main -> ce-23.09@103403)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v23.09/": change = 103404]

--- prj/examples/guides/128-entry-processors/src/main/java/com/oracle/coherence/guides/entryprocessors/utils/CoherenceHelper.java
+++ prj/examples/guides/128-entry-processors/src/main/java/com/oracle/coherence/guides/entryprocessors/utils/CoherenceHelper.java
@@ -1,15 +1,12 @@
 /*
- * Copyright (c) 2022 Oracle and/or its affiliates.
+ * Copyright (c) 2022, 2023 Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
  */
 package com.oracle.coherence.guides.entryprocessors.utils;
 
-import com.oracle.coherence.guides.entryprocessors.model.Country;
 import com.tangosol.net.Coherence;
-import com.tangosol.net.NamedCache;
-import com.tangosol.net.Session;
 
 import java.util.concurrent.CompletableFuture;
 
