9d9195713290a5a36bfc934d00c8a8e21b5e7b27
aleks <aleksandar.seovic@oracle.com>
2023-10-13 23:51:00 +0000

    COH-28595: [35884444->22.06.7] Avoid global virtual index use when executing filter-based entry processors
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 103969]

--- prj/coherence-core/src/main/java/com/tangosol/internal/util/QueryResult.java
+++ prj/coherence-core/src/main/java/com/tangosol/internal/util/QueryResult.java
@@ -1,14 +1,12 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package com.tangosol.internal.util;
 
-
 import com.tangosol.util.Filter;
-
 
 /**
  * QueryResult is a structure that holds the state of the query execution
@@ -18,6 +16,27 @@ import com.tangosol.util.Filter;
  */
 public class QueryResult
     {
+    /**
+     * Construct {@link QueryResult} instance.
+     */
+    public QueryResult()
+        {
+        }
+
+    /**
+     * Construct {@link QueryResult} instance.
+     *
+     * @param aoResult         the array of results (keys or entries) that match the query
+     * @param cResults         the number of results in the array
+     * @param filterRemaining  the filter that still needs to be evaluated
+     */
+    public QueryResult(Object[] aoResult, int cResults, Filter filterRemaining)
+        {
+        this.aoResult = aoResult;
+        this.cResults = cResults;
+        this.filterRemaining = filterRemaining;
+        }
+
     /**
      * The array of results (keys or entries) that match the query.
      */
