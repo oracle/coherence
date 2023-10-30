5ce451652f3ced5ca4097df8b51b41650c868668
Jonathan Knight <jonathan.knight@oracle.com>
2023-10-30 18:12:04 +0000

    Bug 35954328 - [35945522->24.03] Topics channels pause receiving from publishers if a subscriber is killed (merge main -> ce/main 104346) RQ job.9.20231030155032.8893
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 104356]

---
+++ prj/test/functional/topics/src/main/java/topics/bug_35945522/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
+package topics.bug_35945522;
+
+public interface Constants
+    {
+    String TOPIC_NAME = "test";
+
+    String GROUP_NAME = "test-group";
+    }
