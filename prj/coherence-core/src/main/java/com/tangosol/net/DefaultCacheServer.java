421ba5e57c3411f667db7b6f31f48544e4d6ce06
Ryan Lubke <ryan.lubke@oracle.com>
2023-12-02 20:29:46 +0000

    Bug 36067935 - [36067761->14.1.1.0.16] Shutdown hook threads created and registered by DCS will leak if cluster member stopped and started multiple times without restarting the JVM (merge 14.1.1 -> ce/14.1.1 @ 104996)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 104997]

--- prj/coherence-core/src/main/java/com/tangosol/net/DefaultCacheServer.java
+++ prj/coherence-core/src/main/java/com/tangosol/net/DefaultCacheServer.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 package com.tangosol.net;
 
@@ -214,6 +214,10 @@ public class DefaultCacheServer
                 CacheFactory.err(e);
                 }
             }
+
+        // For a clean shutdown, remove the shutdown hook added
+        // when the services are started to avoid leaking threads
+        Runtime.getRuntime().removeShutdownHook(m_threadShutdown);
         }
 
     /**
