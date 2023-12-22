958d918c69782baba29f42d2d3121dabcaae2440
tmiddleton <tim.middleton@oracle.com>
2023-12-22 07:36:45 +0000

    Bug 34343531 - Build: extend.JmxMBeanTests.queryMBean (14.1.1.2206> ce-22.06@105591)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 105596]

--- prj/test/functional/extend/src/main/java/extend/JmxMBeanTests.java
+++ prj/test/functional/extend/src/main/java/extend/JmxMBeanTests.java
@@ -182,7 +182,7 @@ public class JmxMBeanTests
             assertTrue(equals(oMember, service.getCluster().getLocalMember()));
 
             assertTrue("Expected String but got " + oResult, oResult instanceof String);
-            assertTrue(oResult.equals(oMember.toString()));
+            assertTrue("oResult != oMember, oResult=" + oResult, oResult.equals(oMember.toString()));
             }
         finally
             {
@@ -431,7 +431,13 @@ public class JmxMBeanTests
                             }
                         cRetry++;
                         }
-                    while (m_sValue == null && cRetry < 2);
+                    while (m_sValue == null && cRetry < 10);
+                    
+                    if (m_sValue == null)
+                        {
+                        // temp debug: if we get here then we didn't find the MBean after cRetry + 1 tries
+                        setValue("Didn't find MBean in " + (cRetry + 1) + " goes");
+                        }
                     }
                 catch (Exception e)
                     {
