e91255b7dde6f013971acb2802028e47963fbe9d
Jonathan Knight <jonathan.knight@oracle.com>
2024-01-04 11:18:13 +0000

    Bug 36154700 - [36135224->23.09.2] Hostname verification fails due to the SSLSession of the datagram socket not having peer certificates (merge ce/main -> ce/23.09 105727)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v23.09/": change = 105730]

--- prj/coherence-core/src/main/java/com/tangosol/coherence/config/builder/SSLSocketProviderDependenciesBuilder.java
+++ prj/coherence-core/src/main/java/com/tangosol/coherence/config/builder/SSLSocketProviderDependenciesBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -640,12 +640,12 @@ public class SSLSocketProviderDependenciesBuilder
             {
             boolean fMatched = false;
 
-            if (sUrlHostname != null && sslSession != null)
+            if (sUrlHostname != null && !sUrlHostname.isEmpty() && sslSession != null)
                 {
                 Collection<String> colWildcardDNSNames = SSLCertUtility.getDNSSubjAltNames(sslSession, true, false);
                 String             sCertHostname       = SSLCertUtility.getCommonName(sslSession);
 
-                if (colWildcardDNSNames != null && colWildcardDNSNames.size() > 0)
+                if (colWildcardDNSNames != null && !colWildcardDNSNames.isEmpty())
                     {
                     fMatched = VERIFY_CN_AFTER_SAN
                             ? verifySANWildcardDNSNames(sUrlHostname, colWildcardDNSNames)
@@ -663,7 +663,7 @@ public class SSLSocketProviderDependenciesBuilder
                     // non-wildcard SAN DNS Names
                     Collection<String> colSubAltNames = SSLCertUtility.getDNSSubjAltNames(sslSession, false, true);
 
-                    if (colSubAltNames != null && colSubAltNames.size() > 0)
+                    if (colSubAltNames != null && !colSubAltNames.isEmpty())
                         {
                         fMatched = VERIFY_CN_AFTER_SAN
                                 ? doDNSSubjAltNamesVerify(sUrlHostname, colSubAltNames) || doVerify(sUrlHostname, sCertHostname)
