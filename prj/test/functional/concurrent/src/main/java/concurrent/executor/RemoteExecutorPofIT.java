e69f6397d1e1ab0b83855d4a38f708f6423e6434
Ryan Lubke <ryan.lubke@oracle.com>
2024-02-28 22:12:49 -0500

    Bug 36349927 - [36349892->22.06.8] Error creating named executor view when using POF (merge 14.1.1.2206 -> ce/22.06 @ 107208)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 107209]

--- prj/test/functional/concurrent/src/main/java/concurrent/executor/RemoteExecutorPofIT.java
+++ prj/test/functional/concurrent/src/main/java/concurrent/executor/RemoteExecutorPofIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -15,6 +15,7 @@ import com.oracle.bedrock.runtime.coherence.options.LocalHost;
 import com.oracle.bedrock.runtime.coherence.options.LocalStorage;
 import com.oracle.bedrock.runtime.coherence.options.Logging;
 import com.oracle.bedrock.runtime.coherence.options.Multicast;
+import com.oracle.bedrock.runtime.coherence.options.Pof;
 import com.oracle.bedrock.runtime.coherence.options.RoleName;
 
 import com.oracle.bedrock.runtime.java.options.ClassName;
@@ -28,30 +29,28 @@ import com.tangosol.net.Coherence;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
- * Tests to ensure RemoteExecutor can be established over extend.
+ * Tests to ensure RemoteExecutor can be on another cluster member using POF.
  *
- * @author rl   7.12.2021
- * @since 21.12
+ * @author rl  2.28.2024
+ * @since 15.1.1.0.0
  */
-public class RemoteExecutorExtendTestIT
+public class RemoteExecutorPofIT
         extends AbstractRemoteExecutorTest
     {
+    static
+        {
+        System.setProperty("coherence.concurrent.serializer", "pof");
+        }
+
     // ----- AbstractRemoteExecutorTest methods -----------------------------
 
     @Override
     protected Coherence getClient()
         {
-        return Coherence.client();
+        return Coherence.clusterMember();
         }
 
     // ----- data members ---------------------------------------------------
-
-    /**
-     * A Bedrock utility to capture logs of spawned processes into files
-     * under target/test-output. This is added as an option to the cluster
-     * and client processes.
-     */
-    static TestLogs logs = new TestLogs(RemoteExecutorExtendTestIT.class);
 
     /**
      * A Bedrock JUnit5 extension that starts a Coherence cluster of a single
@@ -66,7 +65,7 @@ public class RemoteExecutorExtendTestIT
                           LocalHost.only(),
                           Multicast.ttl(0),
                           IPv4Preferred.yes(),
-                          logs,
+                          SystemProperty.of("coherence.concurrent.serializer", "pof"),
                           ClusterPort.automatic())
                     .include(1,
                              DisplayName.of("storage"),
