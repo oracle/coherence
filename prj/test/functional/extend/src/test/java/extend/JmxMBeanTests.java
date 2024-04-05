754c7beaa0dd2b44d5cfc72adf090bc90aa41090
patrick.fry <patrick.fry@oracle.com>
2024-04-05 12:10:06 -0500

    Bug 35843409 - Build: JmxMBeanTests.testConnectionMessagingDebug (14.1.1.0 cl 108323 --> ce/14.1.1.0)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 108344]

--- prj/test/functional/extend/src/test/java/extend/JmxMBeanTests.java
+++ prj/test/functional/extend/src/test/java/extend/JmxMBeanTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -122,6 +122,7 @@ public class JmxMBeanTests
         {
         InvocationService service = (InvocationService)
             getFactory().ensureService(INVOCATION_SERVICE_NAME);
+        Eventually.assertThat(service.isRunning(), is(true));
 
         try
             {
@@ -301,6 +302,7 @@ public class JmxMBeanTests
                     }
                 catch (Exception e)
                     {
+                    Base.log("JmxMBeanTests.MessagingDebugInvocable.run() got an exception: " + e);
                     throw ensureRuntimeException(e);
                     }
                 }
