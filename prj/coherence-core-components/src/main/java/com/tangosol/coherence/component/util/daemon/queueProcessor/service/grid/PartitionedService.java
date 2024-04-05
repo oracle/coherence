5d70fd254645f79fc32fce1364a3106fc603235c
Vaso Putica <vaso.putica@oracle.com>
2024-04-04 22:59:43 -0500

    Enhancement Request 36481552 - [36431573->24.03.1] ENH: Improve cleaning persistence files on disk to reflect to the cache sizes (merge ce/24.09 -> ce/24.03 @ 108299)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v24.03/": change = 108325]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/PartitionedService.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/PartitionedService.java
@@ -633,6 +633,11 @@ public abstract class PartitionedService
     private int __m_TransferThreshold;
     private static com.tangosol.util.ListMap __mapChildren;
     
+    /**
+     * The interval (in milliseconds) between scheduling two maintenance tasks.
+     */
+    protected static final int MAINTENANCE_INTERVAL = 120000;
+
     // Static initializer
     static
         {
@@ -2297,6 +2302,14 @@ public abstract class PartitionedService
         if (ctrlPart != null)
             {
             ctrlPart.exit();
+            }
+        }
+
+    protected void scheduleEnvironmentMaintenance()
+        {
+        if (isRunning())
+            {
+            getDaemonPool().schedule(new MaintenanceTask(), MAINTENANCE_INTERVAL);
             }
         }
     
@@ -7100,6 +7113,10 @@ public abstract class PartitionedService
         setPartitionEvents(new LocalCache(LocalCache.DEFAULT_UNITS, 3600000));
         
         finalizeStartup();
+        if (isActivePersistence())
+            {
+            scheduleEnvironmentMaintenance();
+            }
         }
     
     // Declared at the super level
@@ -33777,6 +33794,32 @@ public abstract class PartitionedService
                         aControl[iPart].enableTransfer();
                         }
                     }
+                }
+            }
+        }
+
+    // ----- inner class: MaintenanceTask -------------------------------
+
+    /**
+     * Task used to perform necessary maintenance of the underlying environment.
+     */
+    public class MaintenanceTask implements Runnable
+        {
+        // ----- Runnable interface -----------------------------------------
+
+        public void run()
+            {
+            try
+                {
+                PersistenceManager manager = getPersistenceManager();
+                if (manager != null)
+                    {
+                    manager.maintainEnvironment();
+                    }
+                }
+            finally
+                {
+                scheduleEnvironmentMaintenance();
                 }
             }
         }
