02e375d31d4d44aecffc8da97ad5cd2b1252be1b
luk.ho <luk.ho@oracle.com>
2024-10-01 11:14:44 -0500

    Bug 37097260 - [37096406->24.09.1] Cache.getOrDefault() for RWBM does not propagate through to the cache store when an entry does not exist.
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v24.09/": change = 111695]

--- prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/TestCacheStore.java
+++ prj/coherence-testing-support/src/main/java/com/oracle/coherence/testing/TestCacheStore.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package com.oracle.coherence.testing;
@@ -84,6 +84,13 @@ public class TestCacheStore
         {
         log(isVerboseLoad(), "load(" + oKey + ")");
         logMethodInvocation("load");
+
+        // Note: This is for the getOrDefault() test in ReadWriteBackingMapTests.readThroughBasic()
+        // when the entry does not exist.
+        if (!getStorageMap().containsKey(oKey) && oKey.equals("Key10"))
+            {
+            return oKey;
+            }
 
         delay(getDurationLoad());
 
