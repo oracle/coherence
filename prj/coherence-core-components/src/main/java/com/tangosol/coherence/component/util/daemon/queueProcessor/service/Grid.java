0c73987938f616157d16a0653b16c545090126ad
Maurice Gamanho <maurice.gamanho@oracle.com>
2024-12-05 14:01:53 -0500

    Bug 37355950 - [37305744->22.06.11] IllegalStateException: Missing the ownership data for partition 0 from PartitionedService.validatePartitionConfig
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 112737]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/Grid.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/Grid.java
@@ -6906,7 +6906,7 @@ public abstract class Grid
             
             super.onReceived();
             
-            if (!service.isWelcomedBy(memberFrom))
+            if (!service.isWelcomedBy(memberFrom) || mapConfig.isEmpty())
                 {
                 // COH-5774: drop "early" updates.  We must have received a MemberWelcome first.
                 _trace("Ignoring premature ConfigSync from member " + memberFrom.getId(), 5);
