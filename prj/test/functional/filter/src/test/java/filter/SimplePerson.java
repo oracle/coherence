d62f8226b3bed44a2166784c34b8ab5fb2168c00
patrick.fry <patrick.fry@oracle.com>
2024-12-09 13:51:33 -0500

    Bug 37160319 - [37156117->14.1.1.0.20] SafeComparator not honoring NullFirst argument (14.1.1.0 cl 112807 --> ce/14.1.1.0) - fix copyright
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 112823]

--- prj/test/functional/filter/src/test/java/filter/SimplePerson.java
+++ prj/test/functional/filter/src/test/java/filter/SimplePerson.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
 package filter;
 
 import com.tangosol.io.pof.PofReader;
