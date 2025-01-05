b971a28f33b4a6cdbc9c47f3690e9b3239d53638
patrick.fry <patrick.fry@oracle.com>
2025-01-05 10:00:25 -0500

    Enh 37092254 - [36793080->14.1.2.0.1] ENH: Improve the packet publisher packet delivery failure log message to avoid ambiguity. (14.1.2.0 cl 112549 --> ce/14.1.2.0)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 113188]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/ClusterService.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/ClusterService.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -6510,9 +6510,9 @@ public class ClusterService
                         }
                     if (cbPacket > 1468)
                         {
-                        sb.append("It is possible that the packet size greater than ")
-                          .append(cbPacket).append(" is responsible; for example, some network ")
-                          .append("equipment cannot handle packets larger than 1472 bytes (IPv4) or 1468 bytes (IPv6). ")
+                        sb.append("It is possible that the packet size ")
+                                .append(cbPacket).append(" , which is greater than 1468, is responsible; for example, some network ")
+                                .append("equipment cannot handle packets larger than 1468 bytes (UDP) or 1448 bytes (TCP).")
                           .append("Use the 'ping' command with the <size> option to verify successful")
                           .append(" delivery of specifically sized packets.");
                         }
