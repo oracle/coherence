1761c0218a4fc69d045506f99679e739191e6fb8
Chinmay Patel <chinmay.patel@oracle.com>
2025-01-07 14:52:03 -0500

    integ ce/14.1.2 -> ce/main @ 113045 (auto-submit 113242 after successfully running remote remote.full) Job ID: job.9.20250107180605.4739
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 113265]

--- tools/bin/github-deploy-snapshot-docs.sh
+++ tools/bin/github-deploy-snapshot-docs.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-set -e
+
 #
-# Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2000, 2025, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at
-# http://oss.oracle.com/licenses/upl.
+# https://oss.oracle.com/licenses/upl.
 #
 
-CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+set -e
+
+CURRENT_VERSION=$(mvn -f prj help:evaluate -Dexpression=project.version -DforceStdout -q -nsu)
 
 if [ "${CURRENT_VERSION}" = "" ]; then
   echo "Could not find current version from Maven"
