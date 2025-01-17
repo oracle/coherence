6c953f0451ec040dd77e1074dee563020eca044a
Jonathan Knight <jonathan.knight@oracle.com>
2025-01-17 05:19:58 -0500

    COH-31609 Create a proper build pipeline on GitHub - fix 14.1.1.0 snapshot deploy (merge 14.1.1.0 -> ce/14.1.1.0)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 113605]

--- tools/bin/github-deploy-snapshots.sh
+++ tools/bin/github-deploy-snapshots.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
 #
-# Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+# Copyright (c) 2000, 2025, Oracle and/or its affiliates.
 #
 # Licensed under the Universal Permissive License v 1.0 as shown at
 # http://oss.oracle.com/licenses/upl.
 #
 
-CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+REVISION_POM=prj/coherence-bom/pom.xml
+CURRENT_VERSION=$(grep -E "<revision>" ${REVISION_POM} | sed 's/.*<revision>\(.*\)<\/revision>/\1/')
 
 if [ "${CURRENT_VERSION}" = "" ]; then
-  echo "Could not find current version from Maven"
+  echo "Could not find current version from ${REVISION_POM}"
   exit 1
 fi
 
@@ -21,9 +22,7 @@ if [ $? != 0 ] ; then
 fi
 
 echo "Building version ${CURRENT_VERSION}"
-mvn -B clean install -Dproject.official=true -P-modules --file prj/pom.xml -DskipTests -s .github/maven/settings.xml
-mvn -B clean install -Dproject.official=true -Pmodules,-coherence,docs -nsu --file prj/pom.xml -DskipTests -s .github/maven/settings.xml
+mvn -B clean install -Dproject.official=true -Pdocs --file prj/pom.xml -DskipTests -s .github/maven/settings.xml
 
 echo "Deploying version ${CURRENT_VERSION}"
-mvn -B clean deploy -Dproject.official=true -P-modules -nsu --file prj/pom.xml -DskipTests -s .github/maven/settings.xml
-mvn -B clean deploy -Dproject.official=true -Pmodules,-coherence,docs -nsu --file prj/pom.xml -DskipTests -s .github/maven/settings.xml
+mvn -B deploy --file prj/pom.xml -DskipTests -s .github/maven/settings.xml -P-examples
