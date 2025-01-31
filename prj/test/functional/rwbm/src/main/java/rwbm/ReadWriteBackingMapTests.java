d5671984adee26383294235493b87d666b5a2b80
Chinmay Patel <chinmay.patel@oracle.com>
2025-01-31 13:03:37 -0500

    COH-31696: Build: Intermittent failure ReadWriteBackingMapTests.testCacheStoreUpdateOnWriteAsyncPutAll (auto-submit 113769 after successfully running remote remote.full) Job ID: job.9.20250131163600.16616
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 113924]

--- prj/test/functional/rwbm/src/main/java/rwbm/ReadWriteBackingMapTests.java
+++ prj/test/functional/rwbm/src/main/java/rwbm/ReadWriteBackingMapTests.java
@@ -630,7 +630,8 @@ public class ReadWriteBackingMapTests
             }
 
         // stop second member and check that restore/index build gets triggered for partitions coming back
-        stopCacheServer("storage1");
+        stopCacheServer(sServerName);
+
         // wait for server to stop
         Eventually.assertDeferred(() -> cache.getCacheService().getCluster().getMemberSet().size(),
                                   Matchers.is(1), within(5, TimeUnit.MINUTES));
@@ -1940,6 +1941,8 @@ public class ReadWriteBackingMapTests
         store.getStorageMap().clear();
         store.resetStats();
 
+        Eventually.assertDeferred(() -> cache.size(), is(0));
+
         if (store instanceof TestBinaryCacheStore)
             {
             ((TestBinaryCacheStore) store).setProcessor(new AbstractProcessor()
@@ -1964,7 +1967,7 @@ public class ReadWriteBackingMapTests
                     }
                 });
             // non-blocking needs delay
-            cDelay = 5000L;
+            cDelay = 10000L;
             }
 
         try
@@ -1978,6 +1981,8 @@ public class ReadWriteBackingMapTests
                 definiteSleep(cDelay);
                 }
 
+            Eventually.assertDeferred(() -> cache.size(), is(mapData.size()));
+
             for (int i = 0; i < mapData.size(); i++)
                 {
                 Eventually.assertThat(testName,
@@ -1990,7 +1995,7 @@ public class ReadWriteBackingMapTests
                     Eventually.assertDeferred(() -> ExternalizableHelper.isDecorated((Binary)
                                                          rwbm.getInternalCache().get(convDown.convert(ii)),
                                                          ExternalizableHelper.DECO_STORE),
-                                              is (false));
+                                              is(false));
                     }
                 }
             }
