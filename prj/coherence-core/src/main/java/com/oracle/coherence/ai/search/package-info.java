5acea5d1d927808d34a91b4789d2fe0fc7919ee0
Jonathan Knight <jonathan.knight@oracle.com>
2025-02-03 05:23:19 -0500

    Add Coherence AI to JavaDoc (merge main -> ce/main 113925)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 113974]

---
+++ prj/coherence-core/src/main/java/com/oracle/coherence/ai/search/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+/**
+ * Contains classes for Coherence AI vector database Knn search functionality.
+ */
+package com.oracle.coherence.ai.search;
---
+++ prj/coherence-hnsw/src/main/java/com/oracle/coherence/hnswlib/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
+/**
+ * Contains classes for Coherence AI vector database HNSW native proxy.
+ */
+package com.oracle.coherence.hnswlib;
