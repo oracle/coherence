c610ae1338abe2f206414c3362b06c1bcfa4af24
joe.fialli <joe.fialli@oracle.com>
2025-04-15 20:37:09 -0500

    Enhancement Request 37835329 - [37787985->25.09] ENH: Add channel id and next request id to ConnectionException to enable tracing why Channel was closed (main->v25.09) Remote remote.full on coherence-ce/main success, changes 115625, synced @115625, job.9.20250415223700.14098
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 115625]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/net/extend/remoteService/RemoteCacheService.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/net/extend/remoteService/RemoteCacheService.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -514,6 +514,7 @@ public class RemoteCacheService
             Channel channel = cache.getChannel();
             if (channel != null)
                 {
+                _trace(String.format("RemoteCacheService:releaseRemoteNamedCache: channel=%d cacheName=%s", channel.getId(), cache.getCacheName()), 6);
                 channel.close();
                 }
             }
