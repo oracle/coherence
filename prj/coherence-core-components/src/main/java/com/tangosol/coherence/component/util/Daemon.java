306f432614e352d6edb7c8b58b6536413f1bfdab
patrick.fry <patrick.fry@oracle.com>
2025-04-24 15:27:33 -0500

    Bug 37862381 - [37862193->25.09] Increase the severity from 2 to 1 for logging the stack trace when a service is terminated due to an unhandled exception Bug 37862379 - [37862193->15.1.1.0.0] Increase the severity from 2 to 1 for logging the stack trace when a service is terminated due to an unhandled exception Bug 37862378 - [37862193->14.1.2.0.3] Increase the severity from 2 to 1 for logging the stack trace when a service is terminated due to an unhandled exception Bug 37862377 - [37862193->14.1.1.2206.13] Increase the severity from 2 to 1 for logging the stack trace when a service is terminated due to an unhandled exception (14.1.1.0 cl 115829 --> 14.1.1.2206) RQ: job.9.20250423183007.20936 (auto-submit integ 115854 release/coherence-v14.1.1.2206 -> release/coherence-v14.1.2.0 after successfully running remote remote.full) Job ID: job.9.20250424132445.1544 (auto-submit integ 115871 release/coherence-v14.1.2.0 -> main after successfully running remote remote.full) Job ID: job.9.20250424154443.14580 (auto-submit integ 115882 main -> coherence-ce/main after successfully running remote remote.full) Job ID: job.9.20250424182614.20296
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 115898]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/Daemon.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/Daemon.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -973,7 +973,7 @@ public class Daemon
                 {
                 _trace("Terminating " + get_Name() + " due to unhandled exception: "
                     + e.getClass().getName(), 1);
-                _trace(getStackTrace(e), 2);
+                _trace(getStackTrace(e), 1);
                 }
             finally
                 {
