d9513107fca7d98419839638f4f1c8d5cbad94cb
patrick.fry <patrick.fry@oracle.com>
2025-04-27 09:41:55 -0500

    Bug 37872350 - [37724206->14.1.1.0.22] NPE in Daemon.changeState() due to Daemon.getContext() unexpectedly NULL. (14.1.1.0 cl 115944 --> 14.1.1.0 CE)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.1.0/": change = 115960]

--- prj/coherence-core/src/main/java/com/tangosol/net/GuardSupport.java
+++ prj/coherence-core/src/main/java/com/tangosol/net/GuardSupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -646,8 +646,11 @@ public class GuardSupport
         public void release()
             {
             Guardable guardable = getGuardable();
-            guardable.setContext(null);
-            GuardSupport.this.remove(guardable);
+            if (guardable != null)
+                {
+                guardable.setContext(null);
+                GuardSupport.this.remove(guardable);
+                }
             }
 
         /**
