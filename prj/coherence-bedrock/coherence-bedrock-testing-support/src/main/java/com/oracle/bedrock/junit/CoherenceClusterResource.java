84a6f5b8d2b6509b517c2d8eafaeaa3e69945464
Jonathan Knight <jonathan.knight@oracle.com>
2025-06-26 08:07:30 -0500

    Bug 38084318 - Build: Intermittent Topics channel count test failure (merge ce/main -> ce/25.03 117097)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v25.03/": change = 117098]

--- prj/coherence-bedrock/coherence-bedrock-testing-support/src/main/java/com/oracle/bedrock/junit/CoherenceClusterResource.java
+++ prj/coherence-bedrock/coherence-bedrock-testing-support/src/main/java/com/oracle/bedrock/junit/CoherenceClusterResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2023, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -79,7 +79,7 @@ public class CoherenceClusterResource
 
 
     @Override
-    protected void before() throws Throwable
+    public void before() throws Throwable
         {
         if (launchDefinitions.isEmpty())
             {
@@ -100,7 +100,7 @@ public class CoherenceClusterResource
 
 
     @Override
-    protected void after()
+    public void after()
         {
         // clean up the sessions
         synchronized (f_mapCCF)
