0ce07f37d989fd9c9e932e9e621a3712323f1e9f
Ryan Lubke <ryan.lubke@oracle.com>
2025-07-28 16:16:07 -0500

    Bug 38248594 - [38194433->22.06.14] Bump org.slf4j:slf4j* dependencies from 1.7.36 to 2.0.17 (merge 14.1.1.2206 -> ce/22.06 @ 117804)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 117805]

--- prj/test/functional/logging/src/main/java/logging/impl/DebugMDCAdapter.java
+++ prj/test/functional/logging/src/main/java/logging/impl/DebugMDCAdapter.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
  */
 package logging.impl;
 
+import java.util.Deque;
 import java.util.Map;
 
 import org.slf4j.spi.MDCAdapter;
@@ -14,29 +15,64 @@ import org.slf4j.spi.MDCAdapter;
  * This adapter is an empty implementation of the {@link MDCAdapter} interface.
  * It is used for all logging systems which do not support mapped
  * diagnostic contexts such as JDK14, simple, NOP, and debug.
- * 
+ *
  * @author Jason Howes
- * 
  * @since 1.4.1
  */
-public class DebugMDCAdapter implements MDCAdapter {
-  public void clear() {
-  }
+public class DebugMDCAdapter
+        implements MDCAdapter
+    {
+    @Override
+    public void clear()
+        {
+        }
 
-  public String get(String key) {
-    return null;
-  }
+    @Override
+    public String get(String key)
+        {
+        return null;
+        }
 
-  public void put(String key, String val) {
-  }
+    @Override
+    public void put(String key, String val)
+        {
+        }
 
-  public void remove(String key) {
-  }
+    @Override
+    public void remove(String key)
+        {
+        }
 
-  public Map getCopyOfContextMap() {
-    return null;
-  }
+    @Override
+    public Map getCopyOfContextMap()
+        {
+        return null;
+        }
 
-  public void setContextMap(Map contextMap) {
-  }
-}
+    @Override
+    public void setContextMap(Map contextMap)
+        {
+        }
+
+    @Override
+    public void pushByKey(String s, String s1)
+        {
+        }
+
+    @Override
+    public String popByKey(String s)
+        {
+        return "";
+        }
+
+    @Override
+    public Deque<String> getCopyOfDequeByKey(String s)
+        {
+        return null;
+        }
+
+    @Override
+    public void clearDequeByKey(String s)
+        {
+        }
+    }
