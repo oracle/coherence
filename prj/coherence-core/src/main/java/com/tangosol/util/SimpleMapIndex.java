85b96240faced8550935f33084e31cd2bda01eb4
Maurice Gamanho <maurice.gamanho@oracle.com>
2025-07-30 16:46:33 -0500

    Bug 38242066 - [37932122->25.09] RFA: StorageManager.indexInfo MBean reports wrong footprint for some indexes.
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 117946]

--- prj/coherence-core/src/main/java/com/tangosol/util/SimpleMapIndex.java
+++ prj/coherence-core/src/main/java/com/tangosol/util/SimpleMapIndex.java
@@ -542,8 +542,8 @@ public class SimpleMapIndex
                     // (COH-7206)
                     removeInverseMapping(NO_VALUE, oKey, ensureCollection(oIxValueNew));
                     }
-                else if (m_fSplitCollection && oIxValueOld instanceof Collection ||
-                        oIxValueOld instanceof Object[])
+                else if (m_fSplitCollection && (oIxValueOld instanceof Collection ||
+                        oIxValueOld instanceof Object[]))
                     {
                     // Note: it's important to only remove the elements that are no longer
                     //       present in the new value (see COH-7206)
@@ -645,8 +645,8 @@ public class SimpleMapIndex
         // if the extracted value has been already "known", substitute
         // the one we just extracted with a previously extracted one
         // to avoid having multiple copies of equivalent values
-        if (m_fSplitCollection && oIxValue instanceof Collection
-                || oIxValue instanceof Object[])
+        if (m_fSplitCollection && (oIxValue instanceof Collection
+                || oIxValue instanceof Object[]))
             {
             oIxValue = addInverseCollectionMapping(mapInverse, oIxValue, oKey);
             }
