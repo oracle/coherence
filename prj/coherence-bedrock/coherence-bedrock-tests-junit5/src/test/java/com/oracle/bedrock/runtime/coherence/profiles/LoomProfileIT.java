398cdb61b4af1b1d405031b3edbe56068fcb17c3
Jonathan Knight <jonathan.knight@oracle.com>
2025-08-05 12:24:29 -0500

    Fix Bedrock profiles (merge 14.1.2-0 -> ce/14.1.2-0 118180)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 118182]

--- prj/coherence-bedrock/coherence-bedrock-tests-junit5/src/test/java/com/oracle/bedrock/runtime/coherence/profiles/LoomProfileIT.java
+++ prj/coherence-bedrock/coherence-bedrock-tests-junit5/src/test/java/com/oracle/bedrock/runtime/coherence/profiles/LoomProfileIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -13,6 +13,8 @@ import com.oracle.bedrock.runtime.coherence.options.LocalHost;
 import com.oracle.bedrock.runtime.coherence.options.WellKnownAddress;
 import com.oracle.bedrock.runtime.java.options.SystemProperty;
 import com.oracle.bedrock.testsupport.deferred.Eventually;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,9 +22,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LoomProfileIT
     {
+    @BeforeEach
+    public void cleanup()
+        {
+        System.clearProperty(LoomProfile.PROPERTY_ENABLED);
+        }
+
     @Test
     public void shouldEnableLoom()
         {
+        System.setProperty(LoomProfile.PROPERTY_ENABLED, "true");
         LoomProfile profile = new LoomProfile(true);
 
         try (CoherenceClusterMember member = LocalPlatform.get().launch(CoherenceClusterMember.class,
