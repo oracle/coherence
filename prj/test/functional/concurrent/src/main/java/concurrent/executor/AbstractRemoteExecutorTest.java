2576c817bf74522146cdc79e1923a440291311c8
patrick.fry <patrick.fry@oracle.com>
2025-08-07 10:34:23 -0500

    Bug 38286229 - [build] concurrent.executor.RemoteExecutorPofIT.shouldExecuteAndCompleteTask (15.1.1.0 cl 118237 --> 15.1.1.0 CE)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v15.1.1.0/": change = 118254]

--- prj/test/functional/concurrent/src/main/java/concurrent/executor/AbstractRemoteExecutorTest.java
+++ prj/test/functional/concurrent/src/main/java/concurrent/executor/AbstractRemoteExecutorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -63,6 +63,7 @@ public abstract class AbstractRemoteExecutorTest
     static
         {
         System.setProperty("coherence.lambdas", "dynamic");
+        System.setProperty("coherence.executor.trace.logging", "true");
         }
 
     // ----- api ------------------------------------------------------------
