1e7987c25d5557e44b9d96346493a978e9ac5773
aleks <aleksandar.seovic@oracle.com>
2026-03-02 18:09:28 -0500

    Bug 39023815 - [39023803->25.09] OutOfMemoryError when using ArrayFilter
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 119068]

--- prj/test/unit/coherence-tests/src/test/java/com/tangosol/util/filter/AnyFilterTest.java
+++ prj/test/unit/coherence-tests/src/test/java/com/tangosol/util/filter/AnyFilterTest.java
@@ -12,7 +12,11 @@ import org.junit.runner.RunWith;
 
 import com.tangosol.util.MapIndex;
 import com.tangosol.util.Filter;
+import com.tangosol.util.SimpleMapIndex;
+import com.tangosol.util.ValueExtractor;
 import com.tangosol.util.extractor.IdentityExtractor;
+
+import java.lang.reflect.Field;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -20,6 +24,7 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -101,6 +106,127 @@ public class AnyFilterTest
 
         assertTrue("key3 should remain.", setKeys.contains("key3"));
         assertTrue("key2 should remain.", setKeys.contains("key2"));
+        }
+
+    @Test
+    public void shouldReuseOptimizedOrderBetweenCalculateAndApply()
+        {
+        MapIndex index = new SimpleMapIndex(IdentityExtractor.INSTANCE, false, null, null);
+
+        Map<String, Object> map = Map.of(
+                "key1", 1,
+                "key2", 2,
+                "key3", 3,
+                "key4", 4
+        );
+        map.entrySet().forEach(index::insert);
+
+        Map<ValueExtractor, MapIndex> mapIndexes = new HashMap<>();
+        mapIndexes.put(IdentityExtractor.INSTANCE, index);
+
+        CountingFilter counting = new CountingFilter(0);
+        AnyFilter      filter   = new AnyFilter(new Filter[]
+                {
+                counting,
+                new EqualsFilter(IdentityExtractor.INSTANCE, 2)
+                });
+
+        Set<String> setKeys = new HashSet<>(map.keySet());
+
+        filter.calculateEffectiveness(mapIndexes, setKeys);
+        filter.applyIndex(mapIndexes, new HashSet<>(setKeys));
+
+        assertEquals("calculateEffectiveness should be reused between calls.", 1, counting.getCalculateCount());
+        }
+
+    @Test
+    public void shouldClearNestedOptimizedThreadLocalsAfterApply()
+            throws Exception
+        {
+        MapIndex index = new SimpleMapIndex(IdentityExtractor.INSTANCE, false, null, null);
+
+        Map<String, Object> map = Map.of(
+                "key1", 1,
+                "key2", 2,
+                "key3", 3,
+                "key4", 4
+        );
+        map.entrySet().forEach(index::insert);
+
+        Map<ValueExtractor, MapIndex> mapIndexes = new HashMap<>();
+        mapIndexes.put(IdentityExtractor.INSTANCE, index);
+
+        AllFilter filterNested = new AllFilter(new Filter[]
+                {
+                new EqualsFilter(IdentityExtractor.INSTANCE, 2),
+                new EqualsFilter(IdentityExtractor.INSTANCE, 3)
+                });
+
+        AnyFilter filterRoot = new AnyFilter(new Filter[]
+                {
+                filterNested,
+                new EqualsFilter(IdentityExtractor.INSTANCE, 99)
+                });
+
+        Set<String> setKeys = new HashSet<>(map.keySet());
+
+        filterNested.calculateEffectiveness(mapIndexes, setKeys);
+        assertNotNull("Nested optimized order should be set before apply.", getOptimizedThreadLocal(filterNested).get());
+
+        filterRoot.applyIndex(mapIndexes, setKeys);
+
+        assertNull("Root optimized order should be cleared.", getOptimizedThreadLocal(filterRoot).get());
+        assertNull("Nested optimized order should be cleared.", getOptimizedThreadLocal(filterNested).get());
+        }
+
+    private static ThreadLocal<?> getOptimizedThreadLocal(ArrayFilter filter)
+            throws Exception
+        {
+        Field field = ArrayFilter.class.getDeclaredField("f_aFilterOptimized");
+        field.setAccessible(true);
+        return (ThreadLocal<?>) field.get(filter);
+        }
+
+    private static class CountingFilter
+            implements IndexAwareFilter
+        {
+        CountingFilter(int nEffectiveness)
+            {
+            m_nEffectiveness = nEffectiveness;
+            }
+
+        @Override
+        public int calculateEffectiveness(Map mapIndexes, Set setKeys)
+            {
+            m_cCalculate.incrementAndGet();
+            return m_nEffectiveness;
+            }
+
+        @Override
+        public Filter applyIndex(Map mapIndexes, Set setKeys)
+            {
+            return this;
+            }
+
+        @Override
+        public boolean evaluateEntry(Map.Entry entry)
+            {
+            return false;
+            }
+
+        @Override
+        public boolean evaluate(Object o)
+            {
+            return false;
+            }
+
+        int getCalculateCount()
+            {
+            return m_cCalculate.get();
+            }
+
+        private final AtomicInteger m_cCalculate = new AtomicInteger();
+        private final int           m_nEffectiveness;
         }
 
     /**
