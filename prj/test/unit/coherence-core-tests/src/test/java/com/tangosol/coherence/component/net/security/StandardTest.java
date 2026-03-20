063e5a4815bae79d77a74532bd858fc35c1db5c6
sitchapu <shankar.itchapurapu@oracle.com>
2026-03-20 09:08:45 -0500

    COH-33216: backport to CE after remote.full -> https://home.us.oracle.com/internal/coherence/coherence-ce/release/coherence-ce-v14.1.2.0/job.jsp?id=job.9.20260320114934.7006
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 119275]

--- prj/test/unit/coherence-core-tests/src/test/java/com/tangosol/coherence/component/net/security/StandardTest.java
+++ prj/test/unit/coherence-core-tests/src/test/java/com/tangosol/coherence/component/net/security/StandardTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -32,7 +32,7 @@ import static org.mockito.Mockito.verify;
 /**
  * @author jk 2016.04.21
  */
-public class StandardTests
+public class StandardTest
     {
 
     @Test
