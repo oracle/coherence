9bc5373b4b14c61263ff9b3fecd99c06689cbcde
aleks <aleksandar.seovic@oracle.com>
2026-04-21 00:55:05 -0500

    Markdown copyright check
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 119657]

--- prj/etc/copyright-check-enqueue.sh
+++ prj/etc/copyright-check-enqueue.sh
@@ -7,9 +7,10 @@
 #
 echo "Checking copyright for ${1}"
 BASEDIR=$(dirname "$0")
+FILE=${DEV_ROOT}/${1}
 JAR=${DEV_ROOT}/tools/copyright/glassfish-copyright-maven-plugin-2.4-SNAPSHOT.jar
 EXCLUDE="@${BASEDIR}/copyright-exclude.txt"
-CMD="java -cp ${JAR} org.glassfish.copyright.Copyright -c -X ${EXCLUDE} -C ${BASEDIR}/header.txt -A ${BASEDIR}/header-2.txt -A ${BASEDIR}/header-3.txt -q -p4 -noP4fstat $DEV_ROOT/${1}"
+CMD="java -cp ${JAR} org.glassfish.copyright.Copyright -c -X ${EXCLUDE} -C ${BASEDIR}/header.txt -A ${BASEDIR}/header-2.txt -A ${BASEDIR}/header-3.txt -A ${BASEDIR}/header-7.txt -A ${BASEDIR}/header-8.txt -A ${BASEDIR}/header-9.txt -q -p4 -noP4fstat $DEV_ROOT/${1}"
 
 case "${1}" in
     *.md)
