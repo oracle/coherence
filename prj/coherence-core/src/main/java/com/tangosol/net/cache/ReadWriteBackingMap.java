81f303e96812e975f85ee578542b11eaca307f8f
aleks <aleksandar.seovic@oracle.com>
2026-04-25 21:06:17 -0500

    fix: guard write-behind thread service classloader lookup
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 119825]

--- prj/coherence-core/src/main/java/com/tangosol/net/cache/ReadWriteBackingMap.java
+++ prj/coherence-core/src/main/java/com/tangosol/net/cache/ReadWriteBackingMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -4899,7 +4899,7 @@ public class ReadWriteBackingMap
         public void run()
             {
             CacheService service = getCacheService();
-            ClassLoader  loader  = service.getContextClassLoader();
+            ClassLoader  loader  = service == null ? null : service.getContextClassLoader();
             if (loader != null)
                 {
                 setThreadContextClassLoader(loader);
