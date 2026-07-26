ef5797870afd11373b7a7c739483242d34a6104a
patrick.fry <patrick.fry@oracle.com>
2026-07-26 02:31:27 -0500

    integrate 14.1.1.2206 cl 120511 --> 22.06 CE
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v22.06/": change = 121281]

--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/PartitionedCache.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/PartitionedCache.java
@@ -5753,7 +5753,8 @@ public class PartitionedCache
                     try
                         {
                         ctxInvoke.prepareAccess(context,
-                            storage, 0, com.tangosol.net.security.StorageAccessAuthorizer.REASON_INVOKE);
+                            storage, Storage.BinaryEntry.ACCESS_WRITE_ANY,
+                            com.tangosol.net.security.StorageAccessAuthorizer.REASON_INVOKE);
         
                         storage.invoke(ctxInvoke, status, agent);
         
@@ -6441,6 +6442,15 @@ public class PartitionedCache
         Binary binKey = msgRequest.getKey();
         try
             {
+            com.tangosol.net.security.StorageAccessAuthorizer authorizer = storage.getAccessAuthorizer();
+            if (authorizer != null)
+                {
+                com.tangosol.coherence.component.net.RequestContext context = msgRequest.getRequestContext();
+                authorizer.checkWrite(storage.instantiateBinaryEntry(binKey, null, true),
+                    context == null ? null : context.getSubject(),
+                    com.tangosol.net.security.StorageAccessAuthorizer.REASON_LOCK);
+                }
+
             boolean fEnter = isConcurrent();
             if (lockKey(storage, binKey, fEnter))
                 {
@@ -8165,6 +8175,15 @@ public class PartitionedCache
         Binary binKey = msgRequest.getKey();
         try
             {
+            com.tangosol.net.security.StorageAccessAuthorizer authorizer = storage.getAccessAuthorizer();
+            if (authorizer != null)
+                {
+                com.tangosol.coherence.component.net.RequestContext context = msgRequest.getRequestContext();
+                authorizer.checkWrite(storage.instantiateBinaryEntry(binKey, null, true),
+                    context == null ? null : context.getSubject(),
+                    com.tangosol.net.security.StorageAccessAuthorizer.REASON_UNLOCK);
+                }
+
             boolean fEnter = isConcurrent();
             if (lockKey(storage, binKey, fEnter))
                 {
--- prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/partitionedCache/Storage.java
+++ prj/coherence-core-components/src/main/java/com/tangosol/coherence/component/util/daemon/queueProcessor/service/grid/partitionedService/partitionedCache/Storage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2025, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -8783,10 +8783,7 @@ public class Storage
 
         if (checkIndexExists(extractor, comparator, false))
             {
-            if (context != null)
-                {
-                checkAccess(context, BinaryEntry.ACCESS_WRITE_ANY, com.tangosol.net.security.StorageAccessAuthorizer.REASON_INDEX_REMOVE);
-                }
+            checkAccess(context, BinaryEntry.ACCESS_WRITE_ANY, com.tangosol.net.security.StorageAccessAuthorizer.REASON_INDEX_REMOVE);
 
             // remove from each partition index
             for (int iPart = partsMask.next(0); iPart >= 0; iPart = partsMask.next(iPart + 1))
