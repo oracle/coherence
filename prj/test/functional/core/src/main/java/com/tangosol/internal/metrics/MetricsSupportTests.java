abe8f396d55e8e4e978bdc2bd4f845740e7c3730
patrick.fry <patrick.fry@oracle.com>
2026-07-30 15:20:12 -0500

    add journal management observability (14.1.2.0 cl 121102 --> 14.1.2.0 CE)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 121557]

--- prj/test/functional/core/src/main/java/com/tangosol/internal/metrics/MetricsSupportTests.java
+++ prj/test/functional/core/src/main/java/com/tangosol/internal/metrics/MetricsSupportTests.java
@@ -85,7 +85,8 @@ public class MetricsSupportTests
         System.setProperty("coherence.member", "TestClientServer");
         System.setProperty(SiteName.PROPERTY, "TestSite");
         System.setProperty(Logging.PROPERTY_LEVEL, "9");
-        System.setProperty(ClusterName.PROPERTY, "MetricsSupportTestsCluster");
+        System.setProperty(ClusterName.PROPERTY,
+                System.getProperty(ClusterName.PROPERTY, "MetricsSupportTestsCluster"));
         System.setProperty(IPv4Preferred.JAVA_NET_PREFER_IPV4_STACK, "true");
 
         // ensure that all the local services are running before the tests start
