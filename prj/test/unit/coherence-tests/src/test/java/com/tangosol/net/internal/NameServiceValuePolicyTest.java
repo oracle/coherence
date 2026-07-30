8ca50abfd681ca2ef78c7a5820c13f63e73f1295
patrick.fry <patrick.fry@oracle.com>
2026-07-30 15:47:05 -0500

    Integrate commercial 14.1.2.0 CL 121195 into CE
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v14.1.2.0/": change = 121561]

--- prj/test/unit/coherence-tests/src/test/java/com/tangosol/net/internal/NameServiceValuePolicyTest.java
+++ prj/test/unit/coherence-tests/src/test/java/com/tangosol/net/internal/NameServiceValuePolicyTest.java
@@ -80,16 +80,30 @@ public class NameServiceValuePolicyTest
                 "service:jmx:rmi:///jndi/rmi://127.0.0.1:9000/jmxrmi");
         JMXServiceURL urlDirect = new JMXServiceURL(
                 "service:jmx:rmi://127.0.0.1:9000");
+        JMXServiceURL urlIiop = new JMXServiceURL(
+                "service:jmx:iiop://127.0.0.1:9000/jndi/weblogic.management.mbeanservers.runtime");
+        JMXServiceURL urlIiops = new JMXServiceURL(
+                "service:jmx:iiops://127.0.0.1:9000/jndi/weblogic.management.mbeanservers.runtime");
+        JMXServiceURL urlIiopLegacy = new JMXServiceURL(
+                "service:jmx:iiop://127.0.0.1:9000/jndi/iiop://127.0.0.1:9000/weblogic.management.mbeanservers.runtime");
+        JMXServiceURL urlIiopsLegacy = new JMXServiceURL(
+                "service:jmx:iiops://127.0.0.1:9000/jndi/iiops://127.0.0.1:9000/weblogic.management.mbeanservers.runtime");
         JMXServiceURL urlStub = new JMXServiceURL(
                 "service:jmx:rmi://127.0.0.1:9000/stub/abcd");
         JMXServiceURL urlLdap = new JMXServiceURL(
                 "service:jmx:rmi:///jndi/ldap://127.0.0.1:1389/jmxrmi");
         JMXServiceURL urlLdaps = new JMXServiceURL(
                 "service:jmx:rmi:///jndi/ldaps://127.0.0.1:1636/jmxrmi");
-        JMXServiceURL urlIiop = new JMXServiceURL(
+        JMXServiceURL urlIiopProvider = new JMXServiceURL(
                 "service:jmx:iiop:///jndi/iiop://127.0.0.1:9000/jmxrmi");
-        JMXServiceURL urlIiops = new JMXServiceURL(
+        JMXServiceURL urlIiopsProvider = new JMXServiceURL(
                 "service:jmx:rmi:///jndi/iiops://127.0.0.1:9000/jmxrmi");
+        JMXServiceURL urlMalformedIiop = new JMXServiceURL(
+                "service:jmx:iiop://127.0.0.1:9000/jndi/");
+        JMXServiceURL urlT3 = new JMXServiceURL(
+                "service:jmx:t3://127.0.0.1:9000/jndi/weblogic.management.mbeanservers.runtime");
+        JMXServiceURL urlT3s = new JMXServiceURL(
+                "service:jmx:t3s://127.0.0.1:9000/jndi/weblogic.management.mbeanservers.runtime");
         JMXServiceURL urlDns = new JMXServiceURL(
                 "service:jmx:rmi:///jndi/dns://127.0.0.1/jmxrmi");
         JMXServiceURL urlMalformedRmi = new JMXServiceURL(
@@ -97,6 +111,16 @@ public class NameServiceValuePolicyTest
 
         assertSame(url, NameServiceValuePolicy.validateJmxServiceURL(url));
         assertSame(urlDirect, NameServiceValuePolicy.validateJmxServiceURL(urlDirect));
+        assertSame(urlIiop, NameServiceValuePolicy.validateJmxServiceURL(urlIiop));
+        assertSame(urlIiops, NameServiceValuePolicy.validateJmxServiceURL(urlIiops));
+        assertSame(urlIiopLegacy, NameServiceValuePolicy.validateJmxServiceURL(urlIiopLegacy));
+        assertSame(urlIiopsLegacy, NameServiceValuePolicy.validateJmxServiceURL(urlIiopsLegacy));
+        assertSame(urlT3, NameServiceValuePolicy.validateJmxServiceURL(urlT3));
+        assertSame(urlT3s, NameServiceValuePolicy.validateJmxServiceURL(urlT3s));
+        assertSame(urlIiop, NameServiceValuePolicy.validateManagementPublishJmxServiceURL(urlIiop));
+        assertSame(urlIiops, NameServiceValuePolicy.validateManagementPublishJmxServiceURL(urlIiops));
+        assertSame(urlT3, NameServiceValuePolicy.validateManagementPublishJmxServiceURL(urlT3));
+        assertSame(urlT3s, NameServiceValuePolicy.validateManagementPublishJmxServiceURL(urlT3s));
 
         assertThrowsIllegalArgument(() ->
                 NameServiceValuePolicy.validateJmxServiceURL(urlStub));
@@ -105,9 +129,11 @@ public class NameServiceValuePolicyTest
         assertThrowsIllegalArgument(() ->
                 NameServiceValuePolicy.validateJmxServiceURL(urlLdaps));
         assertThrowsIllegalArgument(() ->
-                NameServiceValuePolicy.validateJmxServiceURL(urlIiop));
+                NameServiceValuePolicy.validateJmxServiceURL(urlIiopProvider));
         assertThrowsIllegalArgument(() ->
-                NameServiceValuePolicy.validateJmxServiceURL(urlIiops));
+                NameServiceValuePolicy.validateJmxServiceURL(urlIiopsProvider));
+        assertThrowsIllegalArgument(() ->
+                NameServiceValuePolicy.validateJmxServiceURL(urlMalformedIiop));
         assertThrowsIllegalArgument(() ->
                 NameServiceValuePolicy.validateJmxServiceURL(urlDns));
         assertThrowsIllegalArgument(() ->
