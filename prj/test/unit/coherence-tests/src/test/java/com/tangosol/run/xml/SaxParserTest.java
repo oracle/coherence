ee1d24d82ed5f7bc37b37a0850ad14554585d086
patrick.fry <patrick.fry@oracle.com>
2026-08-03 21:38:00 -0500

    integrate main cl 120929 --> main CE
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 121760]

--- prj/test/unit/coherence-tests/src/test/java/com/tangosol/run/xml/SaxParserTest.java
+++ prj/test/unit/coherence-tests/src/test/java/com/tangosol/run/xml/SaxParserTest.java
@@ -55,6 +55,7 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.XMLReader;
 
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
@@ -365,7 +366,7 @@ public class SaxParserTest
         try (CoherenceModeHelper.ModeScope ignored = CoherenceModeHelper.securityHardened())
             {
             SAXException e = expectSaxException(() -> validateXmlWithSchema(fileSchema));
-            assertThat(e.getMessage(), containsString("accessExternalDTD"));
+            assertExternalDtdBlocked(e);
             }
         }
 
@@ -422,6 +423,14 @@ public class SaxParserTest
             SaxParser.resetForTesting();
             expectSaxException(() -> new SaxParser().parseXml(sXml));
             }
+        }
+
+    private static void assertExternalDtdBlocked(SAXException e)
+        {
+        String sMessage = e.getMessage();
+        assertThat(sMessage, anyOf(
+                containsString("accessExternalDTD"),
+                containsString("CatalogResolver is enabled")));
         }
 
     private void validateXmlWithSchema(File fileSchema)
