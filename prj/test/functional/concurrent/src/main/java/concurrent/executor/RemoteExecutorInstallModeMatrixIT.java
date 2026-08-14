df68bdcdda7cda21813adc2c0f2ce4d0ac42a6d2
patrick.fry <patrick.fry@oracle.com>
2026-08-13 19:29:41 -0500

    stabilize RemoteExecutor install-mode test (15.1.1.0 cl 121596 --> 15.1.1.0 CE)
    [git-p4: depot-paths = "//dev/coherence-ce/release/coherence-ce-v15.1.1.0/": change = 122076]

--- prj/test/functional/concurrent/src/main/java/concurrent/executor/RemoteExecutorInstallModeMatrixIT.java
+++ prj/test/functional/concurrent/src/main/java/concurrent/executor/RemoteExecutorInstallModeMatrixIT.java
@@ -58,6 +58,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.io.DataInput;
 import java.io.DataOutput;
@@ -76,6 +78,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Functional install-mode coverage for RemoteExecutor client paths.
  */
+@Execution(ExecutionMode.SAME_THREAD)
 public class RemoteExecutorInstallModeMatrixIT
     {
     private static final String CLUSTER_NAME = System.getProperty("coherence.cluster",
