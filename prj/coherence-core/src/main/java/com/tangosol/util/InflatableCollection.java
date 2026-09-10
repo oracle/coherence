6ecb5b4470842073b85f72c78b2ea31e7ad18693
patrick.fry <patrick.fry@oracle.com>
2026-09-10 13:48:15 -0500

    Bug 39942912 - RFA: SimpleMapIndex footprint accounting can overcount or undercount after index entry churn (main cl 122278 --> main CE)
    [git-p4: depot-paths = "//dev/coherence-ce/main/": change = 122615]

--- prj/coherence-core/src/main/java/com/tangosol/util/InflatableCollection.java
+++ prj/coherence-core/src/main/java/com/tangosol/util/InflatableCollection.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2000, 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 package com.tangosol.util;
@@ -278,6 +278,16 @@ public abstract class InflatableCollection
 
 
     // ----- helpers -----------------------------------------------------
+
+    /**
+    * Return whether this collection has been inflated.
+    *
+    * @return {@code true} if this collection has been inflated
+    */
+    protected boolean isInflated()
+        {
+        return m_oValue instanceof InflatedCollection;
+        }
 
     /**
     * Factory method used to create a new Collection.
